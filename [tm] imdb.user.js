324b9a734075a1dfb8bcfbdd9f6644737c71774f
Codexophile <jaliya-github@incogneato.us>
2024-10-21 20:51:46 +0530

    imdb - external links - yts, leet

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -1,16 +1,27 @@
-//* External links
-const titleMatch = location.href.match( /\/title\/(tt\d+)/ );
-if ( !titleMatch ) return;
-const titleId = titleMatch[ 1 ];
-const ytsUrlBase = 'https://yts.mx/';
+//#- External links
+
 const el = document.querySelector( `[data-testid="hero__pageTitle"] + ul` );
-const ytsLinkEl = generateElements( `
-    <li class=ipc-inline-list__item>
-        <a target=_blank href=${ ytsUrlBase }browse-movies/${ titleId }>
-            <img style='max-width: 32px' src='https://www.google.com/s2/favicons?sz=64&domain=${ ytsUrlBase }'>
-        </a>
-    </li>` );
-el.appendChild( ytsLinkEl );
+const title = document.querySelector( `[data-testid="hero__pageTitle"]` ).textContent;
+const year = el.querySelector( `[href*="/releaseinfo"]` ).textContent;
+
+//* YTS
+const titleMatch = location.href.match( /\/title\/(tt\d+)/ );
+if ( titleMatch ) {
+    const titleId = titleMatch[ 1 ];
+    addExtLink( 'https://yts.mx/', 'browse-movies/' + titleId );
+}
+
+//* Leet
+addExtLink( 'https://1337x.to/', `search/${ title }+${ year }/1/` );
+
+function addExtLink ( urlBase, urlRest, imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${ urlBase }` ) {
+    generateElements( `
+        <li class=ipc-inline-list__item>
+            <a target=_blank href=${ urlBase }${ urlRest }>
+                <img style='width: 32px' src='${ imgSrc }'>
+            </a>
+        </li>`, el );
+}
 
 const $moreFromSectionEl = $( `[data-testid="more-from-section"]` );
 $moreFromSectionEl.insertBefore( '[data-testid="contribution"]' );
