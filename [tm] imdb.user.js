e23bce9b558c87d8e076ddd34f3a503446458e04
Codexophile <jaliya-github@incogneato.us>
2024-10-23 12:46:22 +0530

    imdb fixes

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -1,26 +1,28 @@
-//#- External links
-
-const el = document.querySelector( `[data-testid="hero__pageTitle"] + ul` );
-const title = document.querySelector( `[data-testid="hero__pageTitle"]` ).textContent;
-const year = el.querySelector( `[href*="/releaseinfo"]` ).textContent;
-
-//* YTS
 const titleMatch = location.href.match( /\/title\/(tt\d+)/ );
 if ( titleMatch ) {
+
+    //#- External links
+
+    const el = document.querySelector( `[data-testid="hero__pageTitle"] + ul` );
+    const title = document.querySelector( `[data-testid="hero__pageTitle"]` ).textContent;
+    const year = el.querySelector( `[href*="/releaseinfo"]` ).textContent;
+
+    //* YTS
     const titleId = titleMatch[ 1 ];
     addExtLink( 'https://yts.mx/', 'browse-movies/' + titleId );
-}
 
-//* Leet
-addExtLink( 'https://1337x.to/', `search/${ title }+${ year }/1/` );
+    //* Leet
+    addExtLink( 'https://1337x.to/', `search/${ title }+${ year }/1/` );
 
-function addExtLink ( urlBase, urlRest, imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${ urlBase }` ) {
-    generateElements( `
+    function addExtLink ( urlBase, urlRest, imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${ urlBase }` ) {
+        generateElements( `
         <li class=ipc-inline-list__item>
             <a target=_blank href=${ urlBase }${ urlRest }>
                 <img style='width: 32px' src='${ imgSrc }'>
             </a>
         </li>`, el );
+    }
+
 }
 
 const $moreFromSectionEl = $( `[data-testid="more-from-section"]` );
