0cc2696a507b94bae2965ad140d5d9f1fcb4ca59
Codexophile <jaliya-github@incogneato.us>
2024-11-02 09:43:53 +0530

    daily.dev: initial

---
+++ [tm] daily.dev.user.js
@@ -0,0 +1,12 @@
+( async function () {
+    'use strict';
+    if ( window.top != window.self ) return; //don't run on frames or iframes
+
+    //* automatically opens the comment section when clicking on an open external button
+    waitForEach( `[href*='/r/']`, ( OpenExternalBtn ) => {
+        OpenExternalBtn.addEventListener( 'click', ( event ) => {
+            OpenExternalBtn.closest( 'article' ).querySelector( 'a' ).click();
+        } );
+    } );
+
+} )();
