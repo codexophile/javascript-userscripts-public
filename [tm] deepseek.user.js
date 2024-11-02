57855542be6ca8d1095c865cb9222d9fc93c925e
Codexophile <jaliya-github@incogneato.us>
2024-11-02 11:56:04 +0530

    deepseek fix

--- [tm] deepseek.user.js
+++ [tm] deepseek.user.js
@@ -3,7 +3,7 @@
     if ( window.top != window.self ) return; //don't run on frames or iframes
 
     waitForEach( 'div:has(>.ds-icon-button)[class]', ( el ) => {
-        GM_setClipboard( `global-document-ready${ document.title }` );
+        GM_setClipboard( `global-document-ready-${ document.title }` );
         if ( document.hidden ) {
             GM_notification( {
                 title: 'DeepSeek',
