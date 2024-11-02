52adc00258b35fc2031f00047969995cf8b814b6
Codexophile <jaliya-github@incogneato.us>
2024-11-02 09:43:07 +0530

    deepseek: initial

---
+++ [tm] deepseek.user.js
@@ -0,0 +1,17 @@
+( function () {
+    'use strict';
+    if ( window.top != window.self ) return; //don't run on frames or iframes
+
+    waitForEach( 'div:has(>.ds-icon-button)[class]', ( el ) => {
+        GM_setClipboard( `global-document-ready${ document.title }` );
+        if ( document.hidden ) {
+            GM_notification( {
+                title: 'DeepSeek',
+                highlight: true,
+                text: 'Ready',
+                timeout: 1000
+            } );
+        }
+    } );
+
+} )();
