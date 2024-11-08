32dacecd282a91c23cdbc5c0c268c3cdbec87dbb
Codexophile <jaliya-github@incogneato.us>
2024-11-08 14:18:56 +0530

    cambridge dictionary

--- [tm] dictionary.cambridge.user.js
+++ [tm] dictionary.cambridge.user.js
@@ -1,22 +1,24 @@
-(function() {
-'use strict';
+( function () {
+    'use strict';
 
-//# Making it possible to copy IPAs and definitions
+    //* Setting audio volume to low
+    const audioEls = document.querySelectorAll( 'audio' );
+    audioEls.forEach( audioEl => {
+        audioEl.volume = 0.1;
+    } );
 
-const ipaSpans = document.querySelectorAll( '.pron.dpron' )
-const defs     = document.getElementsByClassName( 'def' )
+    //* Making it possible to copy IPAs and definitions
+    const ipaSpans = document.querySelectorAll( '.pron.dpron' );
+    const defs = document.getElementsByClassName( 'def' );
+    const $copyBtns = $( `<button> ©️ </button>` ).insertAfter( ipaSpans );
+    const $copyDefBtns = $( `<button> C </button>` ).insertAfter( defs );
+    $copyBtns.on( 'click', ( event ) => {
+        const ipaText = $( event.target ).prev().text().replaceAll( '/', '' );
+        navigator.clipboard.writeText( ipaText );
+    } );
+    $copyDefBtns.on( 'click', ( event ) => {
+        const defText = $( event.target ).prev().text();
+        navigator.clipboard.writeText( defText );
+    } );
 
-const $copyBtns    = $( `<button> ©️ </button>` ).insertAfter( ipaSpans )
-const $copyDefBtns = $( `<button> C </button>` ).insertAfter( defs )
-
-$copyBtns.on( 'click', ( event ) => { 
-    const ipaText = $( event.target ).prev().text().replaceAll( '/', '' )
-    navigator.clipboard.writeText( ipaText )
- } )
-
-$copyDefBtns.on( 'click', ( event ) => {
-    const defText = $( event.target ).prev().text()
-    navigator.clipboard.writeText( defText )
-} )
-
-})();
+} )();
