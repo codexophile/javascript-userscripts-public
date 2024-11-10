bc9cf8f64842ce4d5fbe93278f89bd2b6ca8b22c
Codexophile <jaliya-github@incogneato.us>
2024-11-10 11:31:09 +0530

    convert entire thing to an iif. move video control panel to the side

--- [tm] instagram.user.js
+++ [tm] instagram.user.js
@@ -1,135 +1,151 @@
-//* Expand button
-waitFor( '#collapsibleContent' ).then( ( el ) => {
-    generateToolbarButton( '♒', el, null, () => {
-        if ( !location.href.match( /https:\/\/www\.instagram\.com\/.+?\/$/ ) ) return // 🛑
-        const $main = $( '[style="height: 100%;"]' ).eq( 0 )
-        $main.children().first().hide()
-        $main.children().eq( 1 ).css( `width`, `100%` )
-        const $mainDiv = $( `main > div` ).css( `max-width`, `unset` )
-        const $rowsParent = $mainDiv.children().last().children().first()
-        console.log( $rowsParent )
-        $rowsParent[ 0 ].style = ''
-        style( $rowsParent[ 0 ], `
-            display:       flex;
-            flex-diection: row !important;
-            flex-wrap:     wrap;
-            flex-flow:     unset !important;
-        `)
-        $rowsParent.children().each( function () {
-            unwrap( this )
-        } )
-        addStyle( `
-            main > div > div > div[style] > div { width: 340px }
-        `)
+( function () {
+    'use strict';
 
-        let rowsObserver = new MutationObserver( ( mutations ) => {
-            mutations.forEach( mutation => {
-                mutation.addedNodes.forEach( item => {
-                    if ( $( item ).parent().is( $rowsParent ) ) {
-                        console.log( item )
-                        unwrap( item )
-                    }
-                } )
-            } )
-        } )
-        rowsObserver.observe( $rowsParent[ 0 ], { childList: true, subtree: true } )
+    //* moving video control panel
+    ( async function () {
+        'use strict';
+        const videoControlPanel = await waitFor( '#video-controlPanel' );
+        style( videoControlPanel, `
+                left: unset;
+                right: 100px;
+                top: 50vh;
+            `);
+    } )();
 
-    } )
-} )
+    //* Expand button
+    waitFor( '#collapsibleContent' ).then( ( el ) => {
+        generateToolbarButton( '♒', el, null, () => {
+            if ( !location.href.match( /https:\/\/www\.instagram\.com\/.+?\/$/ ) ) return; // 🛑
+            const $main = $( '[style="height: 100%;"]' ).eq( 0 );
+            $main.children().first().hide();
+            $main.children().eq( 1 ).css( `width`, `100%` );
+            const $mainDiv = $( `main > div` ).css( `max-width`, `unset` );
+            const $rowsParent = $mainDiv.children().last().children().first();
+            console.log( $rowsParent );
+            $rowsParent[ 0 ].style = '';
+            style( $rowsParent[ 0 ], `
+                display:       flex;
+                flex-diection: row !important;
+                flex-wrap:     wrap;
+                flex-flow:     unset !important;
+            `);
+            $rowsParent.children().each( function () {
+                unwrap( this );
+            } );
+            addStyle( `
+                main > div > div > div[style] > div { width: 340px }
+            `);
 
-let observer = new MutationObserver( () => {
+            let rowsObserver = new MutationObserver( ( mutations ) => {
+                mutations.forEach( mutation => {
+                    mutation.addedNodes.forEach( item => {
+                        if ( $( item ).parent().is( $rowsParent ) ) {
+                            console.log( item );
+                            unwrap( item );
+                        }
+                    } );
+                } );
+            } );
+            rowsObserver.observe( $rowsParent[ 0 ], { childList: true, subtree: true } );
 
-    //* Suggested accounts on profile pages
-    const $profilesLocators = $( `[style="width: 170px;"]` )
-    if ( $profilesLocators.length ) {
+        } );
+    } );
 
-        const $grandParent = $( grandParent( $profilesLocators[ 0 ], 6 ) )
-        if ( $grandParent.parent().find( '#profilesWrapper' ).length ) return // 🛑
+    let observer = new MutationObserver( () => {
 
-        const $profilesWrapper = $( `<div id=profilesWrapper></div>` ).insertAfter( $grandParent.prev() )
+        //* Suggested accounts on profile pages
+        const $profilesLocators = $( `[style="width: 170px;"]` );
+        if ( $profilesLocators.length ) {
 
-        $profilesLocators.each( function () {
-            const linkToProfile = this.querySelector( 'a' ).href
-            const porfilePicSrc = this.querySelector( 'img' ).src
-            $profilesWrapper.append( `
-                <a href=${ linkToProfile } style='display: inline-block; width: 33%'>
-                    <img src=${ porfilePicSrc }>
-                </a>
-            `)
-        } )
+            const $grandParent = $( grandParent( $profilesLocators[ 0 ], 6 ) );
+            if ( $grandParent.parent().find( '#profilesWrapper' ).length ) return; // 🛑
 
+            const $profilesWrapper = $( `<div id=profilesWrapper></div>` ).insertAfter( $grandParent.prev() );
+
+            $profilesLocators.each( function () {
+                const linkToProfile = this.querySelector( 'a' ).href;
+                const porfilePicSrc = this.querySelector( 'img' ).src;
+                $profilesWrapper.append( `
+                    <a href=${ linkToProfile } style='display: inline-block; width: 33%'>
+                        <img src=${ porfilePicSrc }>
+                    </a>
+                `);
+            } );
+
+        }
+
+        //* click all 'see translation' button
+        $( `[role=button]:contains('See translation')` ).click();
+
+        // const $imagesOpened = $( '[style*="padding-bottom:"] > img[src]:not(.imgProcessed)' )
+        const queryForIGPosts = 'img[crossorigin="anonymous"][style="object-fit: cover;"]:not(.imgProcessed)';
+        const queryForIGAllImagesItems = '#igAllImages > * > img';
+        const $imagesOpened = $( `${ queryForIGAllImagesItems }, ${ queryForIGPosts }` );
+        $imagesOpened.each( function () {
+
+            this.classList.add( 'imgProcessed' );
+            const $this = $( this );
+            const imgSrc = this.src;
+
+            const $linksContainer = $( `<div></div>` ).insertAfter( $this );
+            style( $linksContainer[ 0 ], `
+                position: absolute;
+                top:      5px;
+                left:     5px;
+                z-index:  1000;
+            `);
+            $linksContainer.append( `<a href='${ imgSrc }' target=_blank> 🔗 </a>` );
+            $( `<button>⬇️</button>` ).appendTo( $linksContainer ).on( 'click', () => clickHandler( this ) );
+
+        } );
+
+        const $videos = $( `video` );
+        $videos.each( function ( index, element ) {
+            // element.pause()
+        } );
+
+    } );
+    observer.observe( document.body, { childList: true, subtree: true } );
+
+    const clickHandler = ( image ) => {
+
+        const tempImg = GM_addElement( 'img', { src: image.src, crossorigin: "anonymous" } );
+        tempImg.addEventListener( 'load', () => {
+
+            var c = $( `canvas` )[ 0 ];
+            c.width = tempImg.naturalWidth;
+            c.height = tempImg.naturalHeight;
+            var ctx = c.getContext( "2d" );
+            ctx.drawImage( tempImg, 0, 0 );
+            const uri = c.toDataURL();
+
+            link = $( `<a></a>` )[ 0 ];
+            let fileName = `${ getUserId( image ) } - ${ getPostId( image ) } - instagram`;
+            link.setAttribute( 'download', `${ fileName }.png` );
+            link.setAttribute( 'href', uri );
+            link.click();
+
+        } );
+    };
+
+    function getUserId ( image ) {
+        let $parent;
+        if ( location.href === 'https://www.instagram.com/' )
+            $parent = $( image ).closest( 'article' );
+        if ( location.href.includes( '/p/' ) )
+            $parent = $( `main` ).first();
+        const userId = $parent.find( '[href^="/"]' ).first().attr( 'href' ).match( /\/(.+?)\// )[ 1 ];
+        console.log( userId );
+        return userId;
     }
 
-    //* click all 'see translation' button
-    $( `[role=button]:contains('See translation')` ).click()
+    function getPostId ( image ) {
+        let href;
+        if ( location.href === 'https://www.instagram.com/' )
+            href = $( image ).closest( 'article' ).find( '[href*="/p/"]' ).attr( 'href' );
+        if ( location.href.includes( '/p/' ) )
+            href = location.href;
+        return href.match( /\/p\/(.+?)(\/|$)/ )[ 1 ];
+    }
 
-    // const $imagesOpened = $( '[style*="padding-bottom:"] > img[src]:not(.imgProcessed)' )
-    const queryForIGPosts = 'img[crossorigin="anonymous"][style="object-fit: cover;"]:not(.imgProcessed)'
-    const queryForIGAllImagesItems = '#igAllImages > * > img'
-    const $imagesOpened = $( `${ queryForIGAllImagesItems }, ${ queryForIGPosts }` )
-    $imagesOpened.each( function () {
-
-        this.classList.add( 'imgProcessed' )
-        const $this = $( this )
-        const imgSrc = this.src
-
-        const $linksContainer = $( `<div></div>` ).insertAfter( $this )
-        style( $linksContainer[ 0 ], `
-            position: absolute;
-            top:      5px;
-            left:     5px;
-            z-index:  1000;
-        `)
-        $linksContainer.append( `<a href='${ imgSrc }' target=_blank> 🔗 </a>` )
-        $( `<button>⬇️</button>` ).appendTo( $linksContainer ).on( 'click', () => clickHandler( this ) )
-
-    } )
-
-    const $videos = $( `video` )
-    $videos.each( function ( index, element ) {
-        // element.pause()
-    } )
-
-} )
-observer.observe( document.body, { childList: true, subtree: true } )
-
-const clickHandler = ( image ) => {
-
-    const tempImg = GM_addElement( 'img', { src: image.src, crossorigin: "anonymous" } )
-    tempImg.addEventListener( 'load', () => {
-
-        var c = $( `canvas` )[ 0 ]
-        c.width = tempImg.naturalWidth
-        c.height = tempImg.naturalHeight
-        var ctx = c.getContext( "2d" )
-        ctx.drawImage( tempImg, 0, 0 )
-        const uri = c.toDataURL()
-
-        link = $( `<a></a>` )[ 0 ]
-        let fileName = `${ getUserId( image ) } - ${ getPostId( image ) } - instagram`
-        link.setAttribute( 'download', `${ fileName }.png` )
-        link.setAttribute( 'href', uri )
-        link.click()
-
-    } )
-}
-
-function getUserId ( image ) {
-    let $parent
-    if ( location.href === 'https://www.instagram.com/' )
-        $parent = $( image ).closest( 'article' )
-    if ( location.href.includes( '/p/' ) )
-        $parent = $( `main` ).first()
-    const userId = $parent.find( '[href^="/"]' ).first().attr( 'href' ).match( /\/(.+?)\// )[ 1 ]
-    console.log( userId )
-    return userId
-}
-
-function getPostId ( image ) {
-    let href
-    if ( location.href === 'https://www.instagram.com/' )
-        href = $( image ).closest( 'article' ).find( '[href*="/p/"]' ).attr( 'href' )
-    if ( location.href.includes( '/p/' ) )
-        href = location.href
-    return href.match( /\/p\/(.+?)(\/|$)/ )[ 1 ]
-}
+} )();
