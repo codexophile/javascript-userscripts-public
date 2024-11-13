39404b94c12df55d9e6b17a941783f961af94ec2
Codexophile <jaliya-github@incogneato.us>
2024-11-13 14:25:59 +0530

    improve code structure

--- [tm] Nomie.user.js
+++ [tm] Nomie.user.js
@@ -1,106 +1,99 @@
-const blackList = [ 'Food', 'Food Generic', 'Habit (Bad)', 'Total Carbohydrate', 'Magnesium', 'Sodium', 'Potassium', 'Calcium',
-  'Iron', 'Total Fat', 'Saturated Fat', 'Protein (Plant)', 'Protein (Animal)', 'Cholesterol', 'Cobalamin', 'Vitamin B', 'Vitamin C',
-  'Caffeine', 'Food Generic'
-]
+( function () {
+    'use strict';
 
-const observer = new MutationObserver( ( mutations ) => {
-  mutations.forEach( mutation => {
-    mutation.addedNodes.forEach( item => {
+    const blackList = [ 'Food', 'Food Generic', 'Habit (Bad)', 'Total Carbohydrate', 'Magnesium', 'Sodium', 'Potassium', 'Calcium',
+        'Iron', 'Total Fat', 'Saturated Fat', 'Protein (Plant)', 'Protein (Animal)', 'Cholesterol', 'Cobalamin', 'Vitamin B', 'Vitamin C',
+        'Caffeine', 'Food Generic'
+    ];
 
-      if ( item.nodeType !== 1 ) return // 🛑
-      if ( !item.matches( '.tiny-trackable:not(.bg-transparent)' ) ) return // 🛑
+    waitForEach( '.tiny-trackable:not(.bg-transparent)', item => {
 
-      // hiding blacklist items
-      const label = item.querySelector( '.label' )
-      const labelText = label.textContent
-      if ( blackList.includes( labelText ) )
-        item.style.display = 'none'
+        // hiding blacklist items
+        const label = item.querySelector( '.label' );
+        const labelText = label.textContent;
+        if ( blackList.includes( labelText ) )
+            item.style.display = 'none';
 
-      // sets the tooltip for the button
-      item.title = label.textContent
-      // hides [.*] from label text
-      label.textContent = label.textContent.replace( /\[.*\]/, '' )
+        // sets the tooltip for the button
+        item.title = label.textContent;
+        // hides [.*] from label text
+        label.textContent = label.textContent.replace( /\[.*\]/, '' );
 
-    } )
-  } )
-} )
-observer.observe( document.body, { childList: true, subtree: true } )
+    } );
 
-//* Right click/Middle click
-$( document.body ).on( 'contextmenu', '.button-wrapper', function ( event ) { clickMore( this, event ) } )
+    //* Right click/Middle click
+    $( document.body ).on( 'contextmenu', '.button-wrapper', function ( event ) { clickMore( this, event ); } );
 
-$( document ).on( "middleclick", ".button-wrapper", function ( event ) {
-  clickMore( this, event )
-  waitFor( '#button-id-4' ).then( ( el ) => { el.click() } )
-} )
+    $( document ).on( "middleclick", ".button-wrapper", function ( event ) {
+        clickMore( this, event );
+        waitFor( '#button-id-4' ).then( ( el ) => { el.click(); } );
+    } );
 
-//* AutoNote
-if ( match = location.href.match( /\?note=(.+)(\?|&|$)/ ) ) {
-  waitFor( 'textarea' ).then( ( el ) => { el.focus() } )
-  waitFor( '#textarea-capture-note' ).then( ( el ) => {
-    textToAdd = decodeURI( match[ 1 ] )
-    textToAdd = textToAdd.replaceAll( '[hash]', '#' )
-    $( el ).val( textToAdd )
-    var eve = new Event( 'input', { bubbles: true, cancelable: true } )
-    document.title = 'Autoclose'
-    el.dispatchEvent( eve )
-    setTimeout( () => {
-      $( '#note-capture' ).find( 'button' ).eq( 5 ).click()
-      waitFor( '.toast-type.success' ).then( ( el ) => { window.close() } )
-    }, 500 )
-  } )
-}
+    //* AutoNote
+    let match;
+    if ( match = location.href.match( /\?note=(.+)(\?|&|$)/ ) ) {
+        waitFor( 'textarea' ).then( ( el ) => { el.focus(); } );
+        waitFor( '#textarea-capture-note' ).then( ( el ) => {
+            textToAdd = decodeURI( match[ 1 ] );
+            textToAdd = textToAdd.replaceAll( '[hash]', '#' );
+            $( el ).val( textToAdd );
+            var eve = new Event( 'input', { bubbles: true, cancelable: true } );
+            document.title = 'Autoclose';
+            el.dispatchEvent( eve );
+            setTimeout( () => {
+                $( '#note-capture' ).find( 'button' ).eq( 5 ).click();
+                waitFor( '.toast-type.success' ).then( ( el ) => { window.close(); } );
+            }, 500 );
+        } );
+    }
 
-function clickMore ( element, event ) {
-  event.preventDefault()
-  $( element ).children( '[aria-label="More about this Trackable"]' ).click()
-}
+    function clickMore ( element, event ) {
+        event.preventDefault();
+        $( element ).children( '[aria-label="More about this Trackable"]' ).click();
+    }
 
-//* Middleclick
-$( document ).on( "mousedown", function ( e1 ) {
-  if ( e1.which === 2 ) {
-    e1.preventDefault()
-    $( document ).one( "mouseup", function ( e2 ) {
-      if ( e1.target === e2.target ) {
-        var e3 = $.event.fix( e2 )
-        e3.type = "middleclick"
-        $( e2.target ).trigger( e3 )
-      }
-    } )
-  }
-} )
+    //* Middleclick
+    $( document ).on( "mousedown", function ( e1 ) {
+        if ( e1.which === 2 ) {
+            e1.preventDefault();
+            $( document ).one( "mouseup", function ( e2 ) {
+                if ( e1.target === e2.target ) {
+                    var e3 = $.event.fix( e2 );
+                    e3.type = "middleclick";
+                    $( e2.target ).trigger( e3 );
+                }
+            } );
+        }
+    } );
 
-//* Minimizing the left bar
+    //* Minimizing the left bar
 
-waitForRecursive()
+    waitForEach( 'aside:not(.fixed)', el => {
 
-function waitForRecursive () {
-  waitFor( 'aside:not(.fixed)' ).then( ( el ) => {
+        $( el ).addClass( 'fixed' );
+        const $aside = $( 'aside' );
+        const $buttonTitles = $( '.mr-2.link-title' ).remove();
+        const $searchButtonTitle = $( 'aside button span:not([class])' ).remove();
 
-    $( el ).addClass( 'fixed' )
-    $aside = $( 'aside' )
-    $buttonTitles = $( '.mr-2.link-title' ).remove()
-    $searchButtonTitle = $( 'aside button span:not([class])' ).remove()
+        $aside.css( 'width', '80px' );
+        $( '[alt="app-logo"]' ).css( 'max-width', 'unset' );
+        const $mainContent = $( '[class="xl:ml-56 layout-section-wrap"]' ).css( 'margin-left', '80px' );
 
-    $aside.css( 'width', '80px' )
-    $( '[alt="app-logo"]' ).css( 'max-width', 'unset' )
-    $mainContent = $( '[class="xl:ml-56 layout-section-wrap"]' ).css( 'margin-left', '80px' )
+    } );
 
-    waitForRecursive()
+    GM_addStyle( `
+    [class="button-wrapper relative w-full flex-grow flex-shrink normal"] {
+      width: 150px;
+    }
+    .item-grid {
+      flex-wrap: wrap;
+      display  : flex;
+    }
+    .title.line-clamp-2 {
+      overflow: hidden !important;
+      white-space: nowrap !important;
+      text-overflow: ellipsis !important;
+    }` );
 
-  } )
-}
 
-GM_addStyle( `
-[class="button-wrapper relative w-full flex-grow flex-shrink normal"] {
-  width: 150px;
-}
-.item-grid {
-  flex-wrap: wrap;
-  display  : flex;
-}
-.title.line-clamp-2 {
-  overflow: hidden !important;
-  white-space: nowrap !important;
-  text-overflow: ellipsis !important;
-}` )
+} )();
