154a7b96ba4844711825c7a3942a060a23d7db37
Codexophile <jaliya-github@incogneato.us>
2024-12-02 11:42:21 +0530

    remove unused code

--- [tm] instagram.user.js
+++ [tm] instagram.user.js
@@ -61,46 +61,6 @@
 
     } );
 
-    return;
-
-    waitFor( '#collapsibleContent' ).then( ( el ) => {
-        generateToolbarButton( '♒', el, null, () => {
-            if ( !location.href.match( /https:\/\/www\.instagram\.com\/.+?\/$/ ) ) return; // 🛑
-            const $main = $( '[style="height: 100%;"]' ).eq( 0 );
-            $main.children().first().hide();
-            $main.children().eq( 1 ).css( `width`, `100%` );
-            const $mainDiv = $( `main > div` ).css( `max-width`, `unset` );
-            const $rowsParent = $mainDiv.children().last().children().first();
-            console.log( $rowsParent );
-            $rowsParent[ 0 ].style = '';
-            style( $rowsParent[ 0 ], `
-                display:       flex;
-                flex-diection: row !important;
-                flex-wrap:     wrap;
-                flex-flow:     unset !important;
-            `);
-            $rowsParent.children().each( function () {
-                unwrap( this );
-            } );
-            addStyle( `
-                main > div > div > div[style] > div { width: 340px }
-            `);
-
-            let rowsObserver = new MutationObserver( ( mutations ) => {
-                mutations.forEach( mutation => {
-                    mutation.addedNodes.forEach( item => {
-                        if ( $( item ).parent().is( $rowsParent ) ) {
-                            console.log( item );
-                            unwrap( item );
-                        }
-                    } );
-                } );
-            } );
-            rowsObserver.observe( $rowsParent[ 0 ], { childList: true, subtree: true } );
-
-        } );
-    } );
-
 
     let observer = new MutationObserver( () => {
 
