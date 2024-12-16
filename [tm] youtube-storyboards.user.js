4336573694b39fc9ad79da75038a2ad0f8d70b5c
Codexophile <jaliya-github@incogneato.us>
2024-12-16 10:03:56 +0530

    add peek buttons. no functionality.

--- [tm] youtube-storyboards.user.js
+++ [tm] youtube-storyboards.user.js
@@ -35,136 +35,48 @@
 
   }
 
-  const modalBoxEl = new modalBox();
+  waitForEach( '#buttonsContainer', ( btnsContainerEl ) => {
+    const peekButton = generateElements( `<button class=peekButton>🫣</button>` );
+    btnsContainerEl.append( peekButton );
+    peekButton.addEventListener( 'click', async () => {
 
-  GM_addStyle( `
+      const videoLinkEl = btnsContainerEl.parentElement.querySelector( 'a' );
+      const videoUrl = videoLinkEl.href;
+      alert( videoUrl );
+      return;
 
-        :is(
-            ytd-rich-item-renderer,
-            ytd-compact-video-renderer,
-            ytd-video-renderer,
-            ytd-playlist-video-renderer > #content
-        ):hover > #buttonsContainer { display: flex }
-        #buttonsContainer { display: none }
+      const ytHtml = await GMXmlHttpReqResponse( videoUrl );
+      const { allUrls, trueNoOfSlots, samplingFq } = generateAllYouTubeSbUrls( ytHtml );
 
-        #buttonsContainer > * {
+      const headerLink = generateElements(
+        `<a href=${ videoUrl } target=_blank> ${ videoLinkEl.textContent } </a>` );
+      const modalBody = generateElements( '<div></div>' );
 
-            width: 30px;
-            height: 25px;
-            line-height: 25px;
-            /* making height = line-height, makes text center vertically */
-            text-align: center;
-            color: white;
-            text-shadow: white 0px 0px 10px;
+      const modal = new ModalBox( {
+        width: '95vw',
+        backgroundColor: '#f0f0f0',
+        headerColor: '#3498db',
+        animation: true,
+        closeOnEscape: true,
+        closeOnOutsideClick: true
+      } );
 
-            display: block;
-            border-radius: 4px;
-            margin: 1px;
-            border: none;
-            background-color: #000000;
-        }
-        #buttonsContainer > *:hover {
-        background: #202020;
-        }
-        #buttonsContainer > *:active {
-            transform: matrix( 0.9, 0, 0, 0.9, 0, 2 );
-        }
-        #peekFullResThumb {
-            text-decoration: none;
-        }
+      console.log( modal );
 
-    ` );
+      modal.setTitle( headerLink );
+      modal.setContent( modalBody );
+      modal.show();
 
-  let observer = new MutationObserver( observerHandler );
-  observer.observe( document.body, { childList: true, subtree: true } );
-
-  function observerHandler () {
-
-    //* adding peek buttons
-
-    let peekParentQuery;
-    let videoLinkWithTitleQuery;
-    if ( location.href.includes( '/results?' ) ) {
-      peekParentQuery = 'ytd-video-renderer';
-      videoLinkWithTitleQuery = 'a#video-title';
-    }
-    if ( location.href.includes( '/watch?v=' ) ) {
-      peekParentQuery = 'ytd-compact-video-renderer';
-      videoLinkWithTitleQuery = 'a:has(#video-title)';
-    }
-    if ( location.href.match( /https:\/\/www.youtube.com\/$/ )
-      // matches: https://www.youtube.com/ 
-      || location.href.match( /\/(@.+?|channel)\// ) ) {
-      // matches https://www.youtube.com/@comedyland573/videos or https://www.youtube.com/channel/UClfq6WEgQm3MG6b30xJhX3g/videos
-      peekParentQuery = `ytd-rich-item-renderer`;
-      videoLinkWithTitleQuery = '#video-title-link';
-    }
-    if ( location.href === 'https://www.youtube.com/playlist?list=WL' )
-      peekParentQuery = 'ytd-playlist-video-renderer > #content';
-
-    const videoThumbs = document.querySelectorAll( peekParentQuery );
-    videoThumbs.forEach( function ( thumb ) {
-
-      if ( thumb.querySelectorAll( '#buttonsContainer' ).length ) return; // 🛑
-
-      const buttonsContainer = generateElements( `<div id=buttonsContainer></div>` );
-      thumb.append( buttonsContainer );
-      buttonsContainer.style = 'position: absolute; left: 5px; top: 5px;';
-
-      if ( !buttonsContainer.querySelectorAll( '.peekButton' ).length ) {
-        const peekButton = generateElements( `<button class=peekButton>🫣</button>` );
-        buttonsContainer.append( peekButton );
-        peekButton.addEventListener( 'click', async () => {
-
-          const videoLink = thumb.querySelector( videoLinkWithTitleQuery );
-          const videoUrl = videoLink.href;
-
-          const ytHtml = await GMXmlHttpReqResponse( videoUrl );
-          const { allUrls, trueNoOfSlots, samplingFq } = generateAllYouTubeSbUrls( ytHtml );
-
-          const headerLink = generateElements(
-            `<a href=${ videoUrl } target=_blank> ${ videoLink.textContent } </a>` );
-          const modalBody = generateElements( '<div></div>' );
-
-          const modal = new ModalBox( {
-            width: '95vw',
-            backgroundColor: '#f0f0f0',
-            headerColor: '#3498db',
-            animation: true,
-            closeOnEscape: true,
-            closeOnOutsideClick: true
-          } );
-
-          console.log( modal );
-
-          modal.setTitle( headerLink );
-          modal.setContent( modalBody );
-          modal.show();
-
-          await storyboard( {
-            storyboardParent: modalBody,
-            horizontal: 5,
-            vertical: 5,
-            linkToVid: videoUrl,
-            trueNoOfSlots,
-            imgUrls: [ ...allUrls ]
-          } );
-
-        } );
-      }
-
-      if ( !thumb.querySelectorAll( '#peekFullResThumb' ).length ) {
-        const videoLink = thumb.querySelector( 'a#thumbnail' );
-        if ( !videoLink ) return;
-        const videoId = videoLink.href.match( /(\/shorts\/|\?v=)(.{11})/ )[ 2 ];
-        const fullResSrc = `https://i.ytimg.com/vi_webp/${ videoId }/maxresdefault.webp`;
-        const peekfullResThumbBtn = generateElements( `<a id=peekFullResThumb href=${ fullResSrc } target=_blank>🖼️</a>` );
-        buttonsContainer.append( peekfullResThumbBtn );
-      }
+      await storyboard( {
+        storyboardParent: modalBody,
+        horizontal: 5,
+        vertical: 5,
+        linkToVid: videoUrl,
+        trueNoOfSlots,
+        imgUrls: [ ...allUrls ]
+      } );
 
     } );
-  }
-
-
+  } );
 
 } )();
