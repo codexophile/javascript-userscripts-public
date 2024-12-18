d7e48c0331cc78e67f8a388ff295850d6459f64f
Codexophile <jaliya-github@incogneato.us>
2024-12-18 10:19:03 +0530

    link to videos on home page works

--- [tm] youtube-item-buttons.user.js
+++ [tm] youtube-item-buttons.user.js
@@ -11,13 +11,6 @@
     addHighResThumbButton( thumbEl, buttonsContainerEl );
   } );
 
-  function createButtonsContainer ( parent ) {
-    const buttonsContainer = generateElements( `<div id=buttonsContainer></div>` );
-    parent.append( buttonsContainer );
-    buttonsContainer.style = 'position: absolute; left: 5px; top: 5px;';
-    return buttonsContainer;
-  }
-
   function addHighResThumbButton ( thumbEl, parentEl ) {
     const videoLink = thumbEl.querySelector( 'a' );
     const videoId = getVideoIdFromLink( videoLink );
@@ -29,29 +22,44 @@
     parentEl.append( btnEl );
   }
 
+  async function addVideosButton ( thumbEl, parentEl ) {
+    const linkToChannel = await getLinkToChannel( thumbEl );
+    const linkToVideos = `${ linkToChannel }/videos`;
+    const newEl = generateElements( `<a>Vid</a>`, parentEl );
+    newEl.href = linkToVideos;
+    newEl.target = '_blank';
+    style( newEl, `
+      text-decoration: none;
+    `);
+    return newEl;
+  }
+
+  async function getLinkToChannel ( thumbEl ) {
+    const linkToChannelEl = thumbEl.querySelector( '[href^="https://www.youtube.com/@"]' );
+    if ( linkToChannelEl ) {
+      const matches = linkToChannelEl.href.match( /@(.+?)(\/|$)/ );
+      if ( !matches ) return null;
+      return `https://www.youtube.com/@${ matches[ 1 ] }`;
+    }
+    const videoLinkEl = thumbEl.querySelector( 'a' );
+    if ( !videoLinkEl ) return null;
+    const videoId = getVideoIdFromLink( videoLinkEl );
+    const channelId = await getLinkToChannel( videoId, API_KEY );
+    return `https://www.youtube.com/channel/${ channelId }`;
+  }
+
+  function createButtonsContainer ( parent ) {
+    const buttonsContainer = generateElements( `<div id=buttonsContainer></div>` );
+    parent.append( buttonsContainer );
+    buttonsContainer.style = 'position: absolute; left: 5px; top: 5px;';
+    return buttonsContainer;
+  }
+
   function getVideoIdFromLink ( linkEl ) {
     const videoUrl = linkEl.href;
     const matches = videoUrl.match( /\/watch\?v=(.{11})/ );
     if ( !matches ) return;
     return matches[ 1 ];
-  }
-
-  async function addVideosButton ( thumbEl, parentEl ) {
-
-    const videoLinkEl = thumbEl.querySelector( 'a' );
-    if ( !videoLinkEl ) return;
-
-    const videoId = getVideoIdFromLink( videoLinkEl );
-    const channelId = await getChannelId( videoId, API_KEY );
-    const channelUrl = `https://www.youtube.com/channel/${ channelId }/videos`;
-
-    const newEl = generateElements( `<a>Vid</a>`, parentEl );
-    newEl.href = channelUrl;
-    newEl.target = '_blank';
-    style( newEl, `
-      text-decoration: none;
-    `);
-
   }
 
 } )();
