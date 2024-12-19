bfd98d36a6ba2f2c75b16ae9a96b995d239ee549
Codexophile <jaliya-github@incogneato.us>
2024-12-19 18:39:39 +0530

    feat: enhance dailyDevHandler to style and rearrange items in the email table

--- [tm] gmail-sender-handlers.user.js
+++ [tm] gmail-sender-handlers.user.js
@@ -3,6 +3,24 @@ function blogtrottrHandler () {
 }
 
 function dailyDevHandler () {
+
+  const itemsParentEl = document.querySelector( 'div[dir="ltr"] > table' );
+  itemsParentEl.classList.add( 'fixedCSS' );
+  style( itemsParentEl, `
+    display: flex;
+    flex-wrap: wrap;
+  `);
+
+  const locatorEls = document.querySelectorAll( '[href*="t.daily.de"][class*=button]' );
+  locatorEls.forEach( locatorEl => {
+    const itemEl = grandParent( locatorEl, 12 );
+    itemsParentEl.prepend( itemEl );
+    style( itemEl, `
+      display: block;
+      width: 45%;
+    `);
+  } );
+
 }
 
 function simklHandler () {
