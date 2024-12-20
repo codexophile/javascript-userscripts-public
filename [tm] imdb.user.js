a9139da61b48ec116885f5b06c42b3b191b3c154
Codexophile <jaliya-github@incogneato.us>
2024-12-20 08:51:36 +0530

    refactor: improve code structure and readability in imdb.user.js

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -1,48 +1,52 @@
 ( function () {
-    'use strict';
+  'use strict';
 
-    //* link fixes
-    let observer = new MutationObserver( () => {
-        $( 'a[href*="?ref"]' ).each( function () {
-            this.href = this.href.replace( /\?ref.*$/, '' );
-        } );
+  //* link fixes
+  let observer = new MutationObserver( () => {
+    $( 'a[href*="?ref"]' ).each( function () {
+      this.href = this.href.replace( /\?ref.*$/, '' );
     } );
-    observer.observe( document.body, { childList: true, subtree: true } );
+  } );
+  observer.observe( document.body, { childList: true, subtree: true } );
 
-    //* External links
+  //* External links
+  addExternalLinks();
 
+
+  const $moreFromSectionEl = $( `[data-testid="more-from-section"]` );
+  $moreFromSectionEl.insertBefore( '[data-testid="contribution"]' );
+
+  //*____________________
+  if ( location.href.includes( 'https://m.' ) ) {
+    location.replace( location.href.replace( 'https://m.', 'https://www.' ) );
+  }
+
+  async function addExternalLinks () {
     const titleMatch = location.href.match( /\/title\/(tt\d+)/ );
 
-    if ( titleMatch ) {
+    if ( !titleMatch )
+      return;
 
-        const el = document.querySelector( `[data-testid="hero__pageTitle"] ~ ul` );
-        const title = document.querySelector( `[data-testid="hero__pageTitle"]` ).textContent;
-        const year = el.querySelector( `[href*="/releaseinfo"]` ).textContent;
+    const el = await waitFor( `[data-testid="hero__pageTitle"] ~ ul` );
+    const title = await waitFor( `[data-testid="hero__pageTitle"]` ).textContent;
+    const year = await waitFor( `[href*="/releaseinfo"]` ).textContent;
 
-        //* YTS
-        const titleId = titleMatch[ 1 ];
-        addExtLink( 'https://yts.mx/', 'browse-movies/' + titleId );
+    //* YTS
+    const titleId = titleMatch[ 1 ];
+    addExtLink( 'https://yts.mx/', 'browse-movies/' + titleId );
 
-        //* Leet
-        addExtLink( 'https://1337x.to/', `search/${ title }+${ year }/1/` );
+    //* Leet
+    addExtLink( 'https://1337x.to/', `search/${ title }+${ year }/1/` );
 
-        function addExtLink ( urlBase, urlRest, imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${ urlBase }` ) {
-            generateElements( `
+    function addExtLink ( urlBase, urlRest, imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${ urlBase }` ) {
+      generateElements( `
             <li class=ipc-inline-list__item>
                 <a target=_blank href=${ urlBase }${ urlRest }>
                     <img style='width: 32px' src='${ imgSrc }'>
                 </a>
             </li>`, el );
-        }
-
     }
 
-    const $moreFromSectionEl = $( `[data-testid="more-from-section"]` );
-    $moreFromSectionEl.insertBefore( '[data-testid="contribution"]' );
-
-    //*____________________
-    if ( location.href.includes( 'https://m.' ) ) {
-        location.replace( location.href.replace( 'https://m.', 'https://www.' ) );
-    }
+  }
 
 } )();
