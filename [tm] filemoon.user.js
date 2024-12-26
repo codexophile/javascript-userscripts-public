eccd12898704e8e16443dbdd4afce9e5ff1ce8cc
Codexophile <jaliya-github@incogneato.us>
2024-12-26 16:35:30 +0530

    refactor: streamline thumbnail handling and improve video element retrieval

--- [tm] filemoon.user.js
+++ [tm] filemoon.user.js
@@ -1,43 +1,39 @@
 ( async function () {
-    'use strict';
+  'use strict';
 
-    //https://filemoon.sx/
-    //https://gaystream.click/
-    //https://vidhideplus.com/
+  //https://filemoon.sx/
+  //https://gaystream.click/
+  //https://vidhideplus.com/
 
-    GM_addStyle( `html, body { overflow: auto !important }` );
+  GM_addStyle( `html, body { overflow: auto !important }` );
 
-    const title = document.title;
-    if ( title ) {
-        const titleEl = generateElements( `<div>${ document.title }</div>` );
-        document.body.prepend( titleEl );
-        style( titleEl, `
+  const title = document.title;
+  if ( title ) {
+    const titleEl = generateElements( `<div>${ document.title }</div>` );
+    document.body.prepend( titleEl );
+    style( titleEl, `
             color: aliceblue;
             font-size: larger;
         `);
-    }
+  }
 
-    let thumbElement;
-    thumbElement = document.querySelector( `.jw-time-thumb` );
-    if ( !thumbElement )
-        thumbElement = await waitFor( '.jw-time-thumb' );
-    alert( thumbElement );
-    const sbUrl = thumbElement.style.backgroundImage.match( /url\("(.+?)"/ )[ 1 ];
-    const $sbParent = $( `<div id=sbMain></div>` ).appendTo( document.body );
-    const vidOnPage = $( 'video' )[ 0 ];
-    vidOnPage.addEventListener( 'loadeddata', () => {
-        alert();
-        const samplingFq = vidOnPage.duration / 100;
-        storyboard( {
-            storyboardParent: $sbParent[ 0 ],
-            horizontal: 3,
-            vertical: 34,
-            vidOnPage,
-            samplingFq,
-            trueNoOfSlots: 100,
-            imgUrls: [ sbUrl ]
-        } );
+  const thumbElement = await waitFor( '.jw-time-thumb' );
+  // alert( 'test' );
+  const sbUrl = thumbElement.style.backgroundImage.match( /url\("(.+?)"/ )[ 1 ];
+  const $sbParent = $( `<div id=sbMain></div>` ).appendTo( document.body );
+  const vidOnPage = await waitFor( 'video' );
+  vidOnPage.addEventListener( 'loadeddata', () => {
+    const samplingFq = vidOnPage.duration / 100;
+    storyboard( {
+      storyboardParent: $sbParent[ 0 ],
+      horizontal: 3,
+      vertical: 34,
+      vidOnPage,
+      samplingFq,
+      trueNoOfSlots: 100,
+      imgUrls: [ sbUrl ]
     } );
+  } );
 
 
 } )();
