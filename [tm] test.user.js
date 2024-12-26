91a02a2a6f58e3b84c920b7ed25c85ab9081ccca
Codexophile <jaliya-github@incogneato.us>
2024-12-26 16:36:23 +0530

    feat: replace GM_xmlhttpRequest with async/await for improved request handling and clipboard functionality

--- [tm] test.user.js
+++ [tm] test.user.js
@@ -4,13 +4,7 @@
 ( async function () {
   'use strict';
 
-  GM_xmlhttpRequest( {
-    method: 'GET',
-    url: 'https://www.gaymaletube.com/out/?l=3AASGc4YWliRq1FDRDY5ZEljMXNpAtl2aHR0cHM6Ly93d3cuYmZodWIuY29tL3ZpZGVvcy8xMjk3MjQ3L2NoaW5lc2UtZnVja2luZy1hdC10aGFpLWdheS1zYXVuYS8/dXRtX3NvdXJjZT1hd24mdXRtX21lZGl1bT10Z3AmdXRtX2NhbXBhaWduPWNwY80BlaJ0YwFFpGRhdGVA2St7ImFsbCI6IiIsIm9yaWVudGF0aW9uIjoiZ2F5IiwicHJpY2luZyI6IiJ9Hc5nakrLqGNhdGVnb3J5zgABkdfA2XxbeyIxIjoiSW0zOUVadDBoSWMifSx7IjIiOiJMdXBBVElyNlhhVCJ9LHsiMyI6IlBxOVRWZnM3UzVjIn0seyItMSI6IkE1eTRTUzQyZUtPIn0seyItMiI6IkJENEYxdWx2bno2In0seyItMyI6IjZkbEc4NFQ3Q0t0In1d&c=894f563f&v=3&',
-    responseType: 'document',
-    onload: function ( response ) {
-      console.log( response );
-    }
-  } );
+  const temp = await GMXmlHttpRequest( 'https://gaydam.net/?v=ksJUz14nY', null, true );
+  GM_setClipboard( temp );
 
 } )();
