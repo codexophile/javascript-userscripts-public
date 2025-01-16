8ac94e4aa4b4b26729a6f029030d7d47f03de3f7
Codexophile <jaliya-github@incogneato.us>
2025-01-16 12:43:14 +0530

    feat: add ytdlp button and improve collapsible toolbar functionality

--- [tm] Global.user.js
+++ [tm] Global.user.js
@@ -1,66 +1,70 @@
 ( async function () {
-    'use strict';
-    if ( window.top != window.self ) return; //don't run on frames or iframes
+  'use strict';
+  if ( window.top != window.self ) return; //don't run on frames or iframes
 
-    //* Beep
-    if ( !document.hidden )
-        GM_setClipboard( `global-document-ready-${ document.title }` );
+  //* Beep
+  if ( !document.hidden )
+    GM_setClipboard( `global-document-ready-${ document.title }` );
 
-    //* toolbar and toolbar buttons
-    const collapsible = await Collapsible( "", {
-        width: "300px",
-        height: "50px",
-        collapsedWidth: "40px",
-    } );
-    collapsible.collapsibleToggler.click();
+  //* toolbar and toolbar buttons
+  const collapsible = await Collapsible( "", {
+    width: "300px",
+    height: "50px",
+    collapsedWidth: "40px",
+  } );
+  collapsible.collapsibleToggler.click();
 
 
-    collapsible.addButton( "🔝", null, () => window.scrollTo( 0, 0 ) );
-    const headersPopup = collapsible.addPopup();
-    collapsible.addButton( "🇭", headersPopup );
-    const iframesPopup = collapsible.addPopup();
-    collapsible.addButton( "ℹ️", iframesPopup );
+  collapsible.addButton( "🔝", null, () => window.scrollTo( 0, 0 ) );
+  const headersPopup = collapsible.addPopup();
+  collapsible.addButton( "🇭", headersPopup );
+  const iframesPopup = collapsible.addPopup();
+  collapsible.addButton( "ℹ️", iframesPopup );
 
-    waitForEach( "h,h1,h2,iframe", ( element ) => {
-        switch ( element.tagName ) {
-            case "H":
-            case "H1":
-            case "H2":
-                generateElements(
-                    `<div>${ element.textContent }</div>`,
-                    headersPopup
-                ).addEventListener( "click", () => {
-                    element.scrollIntoView();
-                } );
-                break;
-            case "IFRAME":
-                generateElements(
-                    `<a href=${ element.src } target=_blank>${ element.src }</a>`
-                );
-                break;
+  waitForEach( "h,h1,h2,iframe", ( element ) => {
+    switch ( element.tagName ) {
+      case "H":
+      case "H1":
+      case "H2":
+        generateElements(
+          `<div>${ element.textContent }</div>`,
+          headersPopup
+        ).addEventListener( "click", () => {
+          element.scrollIntoView();
+        } );
+        break;
+      case "IFRAME":
+        generateElements(
+          `<a href=${ element.src } target=_blank>${ element.src }</a>`
+        );
+        break;
 
-            default:
-                break;
-        }
-    } );
+      default:
+        break;
+    }
+  } );
 
-    collapsible.addButton( '🔊', null, () => {
-        const text = window.getSelection().toString().replaceAll( '\n', '. ' );
-        if ( !text ) return; // 🛑
-        location.href = `edge-tts:${ text }`;
-    } );
+  collapsible.addButton( '🔊', null, () => {
+    const text = window.getSelection().toString().replaceAll( '\n', '. ' );
+    if ( !text ) return; // 🛑
+    location.href = `edge-tts:${ text }`;
+  } );
 
-    const rssLinks = document.querySelectorAll( 'link[rel="alternate"][type="application/rss+xml"], link[rel="alternate"][type="application/atom+xml"]' );
-    if ( rssLinks.length ) {
-        const rssFeedsContainer = collapsible.addPopup();
-        collapsible.addButton( '📶', rssFeedsContainer );
-        rssLinks.forEach( link => {
-            generateElements( `<a
+  const ytdlpBtn = collapsible.addButton( 'ytdlp', null, () => {
+    GM_setClipboard( `initiate-ytdlp::${ document.title }::${ location.href }` );
+  } );
+
+  const rssLinks = document.querySelectorAll( 'link[rel="alternate"][type="application/rss+xml"], link[rel="alternate"][type="application/atom+xml"]' );
+  if ( rssLinks.length ) {
+    const rssFeedsContainer = collapsible.addPopup();
+    collapsible.addButton( '📶', rssFeedsContainer );
+    rssLinks.forEach( link => {
+      generateElements( `<a
                         href='${ link.href }'
                         target=_blank
                         style='display: block;'
                 >${ link.title }</a>`, rssFeedsContainer );
-        } );
-    }
+    } );
+  }
 
 } )();
