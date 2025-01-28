b797bc577083cf910f34cc57d0acbc7dbfe63953
Codexophile <jaliya.chirantha@gmail.com>
2025-01-28 14:29:52 +0530

    feat: improve code formatting for better readability

--- [tm] daily.dev.user.js
+++ [tm] daily.dev.user.js
@@ -1,12 +1,14 @@
 ( async function () {
-    'use strict';
-    if ( window.top != window.self ) return; //don't run on frames or iframes
+  'use strict';
+  if ( window.top != window.self ) return; //don't run on frames or iframes
 
-    //* automatically opens the comment section when clicking on an open external button
-    waitForEach( `[href*='/r/']`, ( OpenExternalBtn ) => {
-        OpenExternalBtn.addEventListener( 'click', ( event ) => {
-            OpenExternalBtn.closest( 'article' ).querySelector( 'a' ).click();
-        } );
+
+
+  //* automatically opens the comment section when clicking on an open external button
+  waitForEach( `[href*='/r/']`, ( OpenExternalBtn ) => {
+    OpenExternalBtn.addEventListener( 'click', ( event ) => {
+      OpenExternalBtn.closest( 'article' ).querySelector( 'a' ).click();
     } );
+  } );
 
 } )();
