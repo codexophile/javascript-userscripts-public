cba846a101ddb70ad0f965c7726b51c87ea774ca
Codexophile <jaliya.chirantha@gmail.com>
2025-02-01 15:48:01 +0530

    feat: implement lazy loading with visual outline for main article elements

--- [tm] daily.dev.user.js
+++ [tm] daily.dev.user.js
@@ -2,7 +2,9 @@
   'use strict';
   if ( window.top != window.self ) return; //don't run on frames or iframes
 
-
+  lazyLoadScrollPast( 'main article', el => {
+    el.style.outline = 'solid red';
+  } );
 
   //* automatically opens the comment section when clicking on an open external button
   waitForEach( `[href*='/r/']`, ( OpenExternalBtn ) => {
