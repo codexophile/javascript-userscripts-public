8e3adb304b4ae7bc12e6528d67a4356f4db01c8e
Codexophile <jaliya.chirantha@gmail.com>
2025-02-01 17:02:07 +0530

    fix: remove hardcoded API key from youtube-title-setter.user.js for security

--- [tm] youtube-title-setter.user.js
+++ [tm] youtube-title-setter.user.js
@@ -1,32 +1,32 @@
 ( function () {
-    'use strict';
-    if ( window.top != window.self ) return; //don't run on frames or iframes
+  'use strict';
+  if ( window.top != window.self ) return; //don't run on frames or iframes
 
-    const API_KEY = 'AIzaSyB41uuRwzZyKBJcMPr-kyNwXBpeOcESOpU';  // Replace this with your YouTube API Key
-    const regionCode = 'US';  // Adjust this according to your preferred region
+  const API_KEY = '';  // Replace this with your YouTube API Key
+  const regionCode = 'US';  // Adjust this according to your preferred region
 
-    main();
+  main();
 
-    async function main () {
+  async function main () {
 
-        const videoId = getVideoId();
-        if ( !videoId ) return;
-        if ( document.title.includes( '{category:' ) ) return;
+    const videoId = getVideoId();
+    if ( !videoId ) return;
+    if ( document.title.includes( '{category:' ) ) return;
 
-        try {
-            const categories = await fetchCategories( regionCode, API_KEY );
-            const categoryAndTags = await getVideoCategoryAndTags( videoId, categories, API_KEY );
-            const newContent = JSON.stringify( categoryAndTags ).replaceAll( '"', '' );
-            const videoTitleEl = await waitFor( '#title.ytd-watch-metadata yt-formatted-string' );
-            const videoTitle = videoTitleEl.innerText;
-            const newTitle = `${ videoTitle } | ${ newContent }`;
-            document.title = newTitle;
-        } catch ( error ) {
-            alert( error );
-        }
+    try {
+      const categories = await fetchCategories( regionCode, API_KEY );
+      const categoryAndTags = await getVideoCategoryAndTags( videoId, categories, API_KEY );
+      const newContent = JSON.stringify( categoryAndTags ).replaceAll( '"', '' );
+      const videoTitleEl = await waitFor( '#title.ytd-watch-metadata yt-formatted-string' );
+      const videoTitle = videoTitleEl.innerText;
+      const newTitle = `${ videoTitle } | ${ newContent }`;
+      document.title = newTitle;
+    } catch ( error ) {
+      alert( error );
     }
+  }
 
-    let titleObserver = new MutationObserver( main );
-    titleObserver.observe( document.querySelector( 'title' ), { childList: true, subtree: true } );
+  let titleObserver = new MutationObserver( main );
+  titleObserver.observe( document.querySelector( 'title' ), { childList: true, subtree: true } );
 
 } )();
