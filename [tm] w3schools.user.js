9d4a86036f2cf64db108fa6ffbbdf9199831cccf
Codexophile <jaliya.chirantha@gmail.com>
2025-02-01 14:11:51 +0530

    feat: wrap code in an IIFE for better scope management

--- [tm] w3schools.user.js
+++ [tm] w3schools.user.js
@@ -1,8 +1,13 @@
-$codeBlocks = $( '.w3-code' )
-const $copyBtns = $( `<button class=copyRole>😊</button>` ).insertAfter( $codeBlocks )
-// $copyBtns.attr( 'style', 'position: absolute; left: 5px; top: 5px;' )
-$copyBtns.on( 'click', function () {
-    $this = $( this )
-    text = $this.siblings( '.w3-code' )[ 0 ].innerText
-    GM_setClipboard( text )
-} )
+( function () {
+  'use strict';
+
+  $codeBlocks = $( '.w3-code' );
+  const $copyBtns = $( `<button class=copyRole>😊</button>` ).insertAfter( $codeBlocks );
+  // $copyBtns.attr( 'style', 'position: absolute; left: 5px; top: 5px;' )
+  $copyBtns.on( 'click', function () {
+    $this = $( this );
+    text = $this.siblings( '.w3-code' )[ 0 ].innerText;
+    GM_setClipboard( text );
+  } );
+
+} )();
