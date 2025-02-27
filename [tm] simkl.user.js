b3d5818b77e7ac5af8790dacc18b588cfd0beaa3
Codexophile <jaliya.chirantha@gmail.com>
2025-02-27 10:04:36 +0530

    feat: add YTS button to simkl.user.js for improved movie browsing experience

--- [tm] simkl.user.js
+++ [tm] simkl.user.js
@@ -1,7 +1,9 @@
-( function () {
+( async function () {
   'use strict';
 
+  const { addElement } = await Collapsible();
 
+  addYtsBtn();
 
   const episodeRegex = /\/season-(\d+)\/episode-(\d+)/;
   const matches = location.href.match( episodeRegex );
@@ -18,5 +20,27 @@
   buttonLeet.onclick = () => {
     window.open( `https://1337x.to/search/${ showName }%20S${ seasonN }E${ episodeN }%20720p/1/` );
   };
+
+  function addYtsBtn () {
+    const imdbId = getImdbId();
+    if ( !imdbId ) return; // 🛑
+
+    const ytsFaviconUrl = `https://www.google.com/s2/favicons?sz=64&domain=yts.mx`;
+    const ytsLinkEl = generateElements( `<a><img src=${ ytsFaviconUrl }></a>` );
+    ytsLinkEl.className = 'button-like';
+    ytsLinkEl.href = `https://yts.mx/browse-movies${ getImdbId() }`;
+    ytsLinkEl.target = '_blank';
+    addElement( ytsLinkEl );
+
+  }
+
+  function getImdbId () {
+    const imdbLinkEl = document.querySelector( `[href^="https://www.imdb.com/title/tt"]` );
+    if ( !imdbLinkEl ) {
+      alert( 'No IMDb link found' );
+      return;
+    }
+    return imdbLinkEl.href.match( /\/tt.+[$\/]/ )[ 0 ];
+  }
 
 } )();
