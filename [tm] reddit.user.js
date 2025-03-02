41b15cfc79ed7057d3f9bcc876c3be95b4f2782f
Codexophile <jaliya.chirantha@gmail.com>
2025-03-02 15:44:31 +0530

    refactor: streamline reddit.user.js by removing unused functions and optimizing event handling

--- [tm] reddit.user.js
+++ [tm] reddit.user.js
@@ -1,81 +1,6 @@
 ( function () {
   'use strict';
 
-  let filterList = GM_getValue( 'filterList', [] );
-  main();
-  window.addEventListener( 'urlchange', main );
-
-  async function main () {
-
-    let $activePost;
-
-    if ( location.href.includes( '/comments/' ) ) return;
-
-    const mainEl = await waitFor( 'main:not(.scrollEvAdded)' );
-    mainEl.classList.add( 'scrollEvAdded' );
-
-    mainEl.parentElement.parentElement.addEventListener( 'wheel', ( event ) => {
-      if ( event.altKey ) return;
-      event.preventDefault();
-
-      let $scrollTo;
-      if ( !$activePost?.length ) {
-        $activePost = jQuery( `article` ).first();
-        markAsActive( $activePost[ 0 ] );
-        $scrollTo = $activePost;
-      }
-      else {
-        if ( event.deltaY > 0 ) {
-          $scrollTo = $activePost.nextAll( 'article' ).first();
-          if ( !$scrollTo.length )
-            $scrollTo = $activePost.nextAll().find( 'article ' ).first();
-          if ( !$scrollTo.length )
-            $scrollTo = $activePost.parent().nextAll( 'faceplate-batch' ).find( 'article ' ).first();
-        }
-        if ( event.deltaY < 0 ) {
-          $scrollTo = $activePost.prevAll( 'article' ).first();
-          if ( !$scrollTo.length )
-            $scrollTo = $activePost.parent().prevAll( 'article' ).first();
-          if ( !$scrollTo.length )
-            $scrollTo = $activePost.parent().prevAll( 'faceplate-batch' ).first().find( 'article ' ).last();
-        }
-      }
-      if ( !$scrollTo.length ) return; // 🛑
-
-      $scrollTo[ 0 ].scrollIntoView( { block: 'center', behaviour: 'smooth' } );
-
-      const formerPostId = $activePost.children().attr( 'permalink' ).match( /\/comments\/(.+?)\// )[ 1 ];
-      filterList.push( formerPostId );
-      filterList = [ ...new Set( filterList ) ];
-      GM_setValue( 'filterList', filterList );
-
-      markAsActive( $scrollTo[ 0 ], $activePost[ 0 ] );
-
-      function markAsActive ( el, formerEl ) {
-        if ( formerEl ) formerEl.style.outline = '';
-        el.style.outline = 'solid red';
-        $activePost = jQuery( el );
-      }
-
-    } );
-
-
-    function scrollToNext () {
-
-    }
-    function scrollToPrev () {
-      if ( !$activePost )
-        $activePost = document.querySelector( `article` );
-      const nextPost = $activePost.nextElementSibling.nextElementSibling;
-      nextPost.scrollIntoView( { block: 'center' } );
-      $activePost.style.outline = '';
-      nextPost.style.outline = 'solid red';
-      $activePost = nextPost;
-    }
-
-
-  }
-
   waitFor( '.collapsible-content' ).then( async ( el ) => {
 
     // el.parentElement.style.left = '';
@@ -108,39 +33,10 @@
       blockAnchor( topAllLink, 'TopAll' );
     }
 
-    if ( !location.href.includes( '/comments/' ) ) {
-      generateToolbarButton( '⬆️', el, null, () => {
-      } );
-      generateToolbarButton( '⬇️', el, null, () => {
-      } );
-    }
-
-    const $filteredCountDiv = jQuery(
-      `<div id=filteredCountDiv style='color:black'>F</div>` );
-    $filteredCountDiv.appendTo( el );
-
   } );
 
-  let filteredCount = 0;
 
   let observer = new MutationObserver( () => {
-
-    //* filtering
-
-    const allArticles = jQuery( 'article:not(.filterDone)' )
-      .addClass( 'filterDone' ).each( function () {
-        const $this = jQuery( this );
-        const permalink = $this.children().attr( 'permalink' );
-        const title = $this.attr( 'aria-label' );
-        const articleId = permalink.match( /\/comments\/(.+?)\// )[ 1 ];
-        if ( filterList.includes( articleId ) ) {
-          filteredCount++;
-          jQuery( '#filteredCountDiv' ).text( filteredCount );
-
-          $this.replaceWith( `<div><h3>Filtered</h3><a target=_blank href=${ permalink }>${ title }</a></div>` );
-          // $this.remove()
-        }
-      } );
 
     //* gallery
     jQuery( 'gallery-carousel:not(.galleryDone)' ).each( function () {
@@ -174,33 +70,9 @@
       } );
     } );
 
-    //* Adding an anchor element with src to each <img> element so the imagus extension can catch it easily
-    jQuery( `:is(shreddit-post,[data-testid="post-container"]) img:not(.imgDone)` ).each( function () {
-      // 'shreddit-post' is for the 'new new' reddit ui. 
-      // [data-testid="poste-container"] is for the 'old new' reddit ui
-      let $this = jQuery( this );
-      $this.after( `<a href=${ this.src } style='
-                position: absolute;
-                right   : 0px;
-                bottom  : 0px;
-            '> Img </a>`);
-      this.classList.add( 'imgDone' );
-    } );
-
     //* Enabling controls for "gif" video elements
     //? Only applicable to the 'new' new reddit UI
     jQuery( `[gif]` ).removeAttr( 'gif' );
-
-    //* Wrapping a link around each post title
-    //? Only applicable to the 'new' new reddit UI
-    jQuery( `shreddit-post > [id*=post-title-]` ).each( function () {
-      let $this = jQuery( this );
-      let $siblingAnchor = $this.siblings( 'a' );
-      let postHref = $siblingAnchor.attr( 'href' );
-      let $wrapper = jQuery( `<a href=${ postHref } class=wrapper-anchor target=_blank></a>` );
-      $this.wrap( $wrapper );
-      //// siblingAnchor.remove() // to prevent the link being clicked when trying to interact with the video
-    } );
 
     //* Reddit old links
     let $links = jQuery( '[href^="/r/"]:not(.wwwToOldDone)' );
