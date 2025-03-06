a768b4325da27bbdc6bdb93e1a4d57dbcdc6cb9e
Codexophile <jaliya.chirantha@gmail.com>
2025-03-06 19:27:26 +0530

    feat: add function to fetch channel country of origin and related API calls

--- [fun] youtube.js
+++ [fun] youtube.js
@@ -11,10 +11,16 @@ function getYoutubeAPI () {
 
 }
 
-// Function to extract video ID from the URL
 function getVideoId () {
   const urlParams = new URLSearchParams( window.location.search );
   return urlParams.get( 'v' );
+}
+
+async function fetchChannelDetails ( channelId, API_KEY ) {
+  const apiUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,brandingSettings&id=${ channelId }&key=${ API_KEY }`;
+  const response = await GMXmlHttpRequestAsync( apiUrl );
+  const data = JSON.parse( response );
+  return data;
 }
 
 async function fetchVideoDetails ( videoId, API_KEY ) {
@@ -29,7 +35,6 @@ async function getChannelId ( videoId, API_KEY ) {
   return data.items[ 0 ].snippet.channelId;
 }
 
-// Function to fetch video details (category and tags)
 async function getVideoCategoryAndTags ( videoId, categories, API_KEY ) {
   data = await fetchVideoDetails( videoId, API_KEY );
   const categoryId = data.items[ 0 ].snippet.categoryId;
@@ -38,7 +43,6 @@ async function getVideoCategoryAndTags ( videoId, categories, API_KEY ) {
   return { category, tags };
 }
 
-// Function to fetch available video categories from YouTube Data API
 async function fetchCategories ( regionCode, API_KEY ) {
   const apiUrl = `https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=${ regionCode }&key=${ API_KEY }`;
   const response = await GMXmlHttpRequestAsync( apiUrl );
@@ -50,4 +54,26 @@ async function fetchCategories ( regionCode, API_KEY ) {
     } );
   }
   return categories;
+}
+
+async function getChannelCountryOfOrigin ( channelId, API_KEY ) {
+  const data = await fetchChannelDetails( channelId, API_KEY );
+
+  if ( data.items && data.items.length > 0 ) {
+    // Try to get country from brandingSettings first
+    if ( data.items[ 0 ].brandingSettings &&
+      data.items[ 0 ].brandingSettings.channel &&
+      data.items[ 0 ].brandingSettings.channel.country ) {
+      return data.items[ 0 ].brandingSettings.channel.country;
+    }
+
+    // Fallback to snippet.country if available
+    if ( data.items[ 0 ].snippet && data.items[ 0 ].snippet.country ) {
+      return data.items[ 0 ].snippet.country;
+    }
+
+    return "Country not specified";
+  }
+
+  return "Channel not found";
 }
