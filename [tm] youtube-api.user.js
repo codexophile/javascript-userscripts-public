b8bda8abc0937949c31aeff38eaa72b2f6dee48f
Codexophile <jaliya.chirantha@gmail.com>
2025-03-07 13:46:11 +0530

    feat: add YouTube API functions for fetching channel and video details with caching

---
+++ [tm] youtube-api.user.js
@@ -0,0 +1,14 @@
+( function () {
+  'use strict';
+  if ( window.top != window.self ) return; //don't run on frames or iframes
+
+  const API_KEY = getYoutubeAPI();
+
+  // const videoId = getVideoId();
+  // const channelId = await getChannelId( videoId, API_KEY );
+
+  // const countryOfOrigin = await getChannelCountryOfOrigin( channelId, API_KEY );
+  // alert( countryOfOrigin );
+
+
+} )();
