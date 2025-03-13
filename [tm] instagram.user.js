9da1359dc13053a2a627ef58af3d11d3dd97ad78
Codexophile <jaliya.chirantha@gmail.com>
2025-03-13 18:03:30 +0530

    refactor: comment out markAndFilter function and adjust video control panel positioning logic

--- [tm] instagram.user.js
+++ [tm] instagram.user.js
@@ -1,18 +1,18 @@
 ( async function () {
   'use strict';
 
-  markAndFilter( 'main div:has(>[href^="/p/"])', 'a', 'href', /\/p\/(.+?)\// )
+  // markAndFilter( 'main div:has(>[href^="/p/"])', 'a', 'href', /\/p\/(.+?)\// )
 
-    //* moving video control panel
-    ( async function () {
-      'use strict';
-      const videoControlPanel = await waitFor( '#video-controlPanel' );
-      style( videoControlPanel, `
+  //* moving video control panel
+  ( async function () {
+    'use strict';
+    const videoControlPanel = await waitFor( '#video-controlPanel' );
+    style( videoControlPanel, `
                 left: unset;
                 right: 100px;
                 top: 50vh;
             `);
-    } )();
+  } )();
 
   //* Expand button
   const collapsibleObj = await Collapsible();
