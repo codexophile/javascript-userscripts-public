2d3e042075881923744e1c7706b70badeeaf8908
Codexophile <jaliya.chirantha@gmail.com>
2025-03-24 12:57:06 +0530

    feat: add modal functionality for clearing chat and downloading images with descriptions in AiStudio

--- [tm] aistudio.google.user.js
+++ [tm] aistudio.google.user.js
@@ -1,17 +1,50 @@
-( function () {
+( async function () {
   'use strict';
   if ( window.top != window.self ) return; //don't run on frames or iframes
+
+  const modalObj = new ModalBox();
+  const modalContentEl = generateElements( `<div></div>` );
+  modalObj.setContent( modalContentEl );
+
+  const { addButton } = await Collapsible();
+
+  addButton( '🧹', null, async () => {
+    const clearChatBtnEl = document.querySelector( `button[aria-label="Clear chat"]` );
+    clearChatBtnEl.click();
+    const continueBtn = await waitFor( 'mat-dialog-actions > [color="primary"]' );
+    continueBtn.click();
+  } );
+
+  addButton( '🖼️', null, async () => {
+    modalObj.show();
+  } );
+
+  downloadImgWithTextFunctionality( {
+    siteName: 'AiStudio',
+    imageElSelector: '[alt^="Generated Image"]',
+    getDescription ( imgEl ) {
+      const grandParentEl = imgEl.closest( 'ms-chat-turn' );
+      const descriptionEl = prev( grandParentEl ).querySelector( '.user-prompt-container' );
+      return descriptionEl.textContent;
+    },
+  } );
 
   waitForEach( '[mattooltip="Good response"]', ( el ) => {
     // if ( !document.hidden ) return;
     GM_setClipboard( `global-document-ready-${ document.title }` );
     GM_notification( {
-      title: 'DeepSeek',
+      title: 'AiStudio',
       highlight: true,
       text: 'Ready',
       timeout: 1000
     } );
 
+  } );
+
+  hotkeys( 'alt+r', ( event, handler ) => {
+    const rerunBtnEls = document.querySelectorAll( '[name="rerun-button"]' );
+    const lastRerunBtnEl = rerunBtnEls[ rerunBtnEls.length - 1 ];
+    lastRerunBtnEl.click();
   } );
 
   hotkeys( 'alt+e', ( event, handler ) => {
