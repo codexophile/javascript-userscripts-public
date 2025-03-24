5c2c5ad56091f2515986c2e986f4cd7e7129f7ce
Codexophile <jaliya.chirantha@gmail.com>
2025-03-24 11:52:45 +0530

    feat: automate clicking on new post notifications and show button for censored tweets

--- [tm] x.user.js
+++ [tm] x.user.js
@@ -1,16 +1,16 @@
 ( function () {
-    'use strict';
+  'use strict';
 
-    let observer = new MutationObserver( () => {
+  // Automatically clicking 'New post notifications for ' item
+  const notifQuery = '[data-testid="notification"]';
+  waitForEach( notifQuery, ( notifEl ) => {
+    if ( notifEl.textContent.includes( "New post notifications for " ) ) {
+      notifEl.click();
+    }
+  } );
 
-        // Automatically clicking "show" button on censored tweets
-        $( '[role=article] [role=button]:contains("Show"), [role=button] > span:contains("Show")' ).click();
-
-        // Automatically clicking 'New post notifications for ' item
-        if ( !window.location.href.includes( "#notif" ) ) return;
-        $( '[data-testid="notification"]:contains("New post notifications for ")' ).click();
-
-    } );
-    observer.observe( document.body, { childList: true, subtree: true } );
+  // Automatically clicking "show" button on censored tweets
+  const query = '[role=article] [role=button]:contains("Show"), [role=button] > span:contains("Show")';
+  waitForEach( query, ( showEl ) => showEl.click() );
 
 } )();
