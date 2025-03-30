19e76bb5db88ce8cc43a342e0b591c047a186dda
Codexophile <jaliya.chirantha@gmail.com>
2025-03-30 07:55:42 +0530

    feat: add click event listener to yt-dlp button for video URL copying

--- [tm] streamwish.user.js
+++ [tm] streamwish.user.js
@@ -1,4 +1,22 @@
 ( async function () {
+  'use strict';
+
+  ( async function () {
+    'use strict';
+
+    let ytDlpBtnEl = await waitFor( '#yt-dlp-btn' );
+    ytDlpBtnEl = removeListenersByCloning( ytDlpBtnEl );
+    ytDlpBtnEl.addEventListener( 'click', () => {
+      const videoUrl = document.querySelector( `#titleM3u8Link` )?.href;
+      if ( !videoUrl ) alert( 'No video URL found' );
+      const videoTitle = document.title.replace( ' • [Browser:Private-profile]', '' );
+      GM_setClipboard( `initiate-ytdlp:url:${ videoUrl }::title:${ videoTitle }::` );
+    } );
+
+  } )();
+
+
+  ( async function () {
     'use strict';
 
     let storyboardUrl;
@@ -13,19 +31,19 @@
 
     let parent;
     if ( $( '.videoplayer, #vplayer' ).length )
-        parent = $( `<div></div>` ).insertAfter( $( '.videoplayer, #vplayer' ).first() );
+      parent = $( `<div></div>` ).insertAfter( $( '.videoplayer, #vplayer' ).first() );
     else
-        parent = $( `<div></div>` ).appendTo( document.body );
+      parent = $( `<div></div>` ).appendTo( document.body );
 
     const storyboardEl = await storyboard( {
-        storyboardParent: parent[ 0 ],
-        horizontal: 10, vertical: 10,
-        vidOnPage: $videoEl[ 0 ],
-        trueNoOfSlots: 100,
-        imgUrls: [ storyboardUrl ]
+      storyboardParent: parent[ 0 ],
+      horizontal: 10, vertical: 10,
+      vidOnPage: $videoEl[ 0 ],
+      trueNoOfSlots: 100,
+      imgUrls: [ storyboardUrl ]
     } );
 
-    storyboardEl.scrollIntoView();
+  } )();
 
 
 } )();
