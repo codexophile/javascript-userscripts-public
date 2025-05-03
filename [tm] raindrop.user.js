f6eb8c45946b24da1e85a7da6fbaf2c077c6df68
Codexophile <jaliya.chirantha@gmail.com>
2025-05-03 20:51:43 +0530

    feat: add user script to extract and display product prices from Temu

---
+++ [tm] raindrop.user.js
@@ -0,0 +1,46 @@
+(function () {
+  "use strict";
+  if (window.top != window.self) return; //don't run on frames or iframes
+
+  const shoppingSites = ["https://www.temu.com", "https://www.aliexpress.com"];
+
+  waitForEach("main:not([class])>div", async (bookmarkEl) => {
+    const link = bookmarkEl.querySelector("a[class*=permalink]").href;
+    const isShoppingSite = shoppingSites.some((site) => link.includes(site));
+    const aboutSectionEl = bookmarkEl.querySelector("div[class*=about]");
+    if (!isShoppingSite) return;
+
+    if (link.includes("temu.com")) {
+      const html = await GMXmlHttpRequest(link, null, true);
+
+      const discountedPriceMatches = html.match(/Only LKR (.+?) with extra/);
+      const discountedPrice = discountedPriceMatches
+        ? discountedPriceMatches[1]
+        : null;
+      if (discountedPrice) {
+        generateElements(
+          `<div>Disc. Price: ${discountedPrice}</div>`,
+          aboutSectionEl
+        );
+        return;
+      }
+
+      const originalPriceMatches = html.match(
+        /"minOnSalePriceStr": "LKR (.+?)"/
+      );
+      console.log(originalPriceMatches);
+      const originalPrice = originalPriceMatches
+        ? originalPriceMatches[1]
+        : null;
+      if (originalPrice) {
+        generateElements(
+          `<div>Original Price: ${originalPrice}</div>`,
+          aboutSectionEl
+        );
+        return;
+      }
+
+      generateElements(`<div>Price not found</div>`, aboutSectionEl);
+    }
+  });
+})();
