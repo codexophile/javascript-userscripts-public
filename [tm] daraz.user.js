9b5475cdf0d0ba61bd435493f0143b664de3133d
Codexophile <jaliya.chirantha@gmail.com>
2025-06-14 12:43:06 +0530

    feat: enhance product image loading and improve styling for product items

--- [tm] daraz.user.js
+++ [tm] daraz.user.js
@@ -18,13 +18,38 @@
     productItemEl.append(imageContEl);
 
     thumbImgEls.forEach((thumbImgEl) => {
+      const src = thumbImgEl.src;
+      const hiresSrc = src.replace(/_\d+x\d+/, "_720x720");
+      if (src !== hiresSrc) {
+        thumbImgEl.src = hiresSrc;
+      }
+
+      // if new scr fails loading, fallback to original src
+      thumbImgEl.onerror = () => {
+        thumbImgEl.src = src;
+      };
+    });
+
+    thumbImgEls.forEach((thumbImgEl) => {
       style(
         thumbImgEl,
         `
         margin: 3px;
-        max-width: 300px;`
+        max-width: 150px;`
       );
     });
-    style(productItemEl, `display: flex;`);
+
+    // give productItemEl a card-like appearance
+    // prettify the product item element
+    style(
+      productItemEl,
+      `
+      display: flex;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      padding: 10px;
+      margin: 10px;
+      background-color: #fff;`
+    );
   }, ...productItemEls);
 })();
