000e80a43a801c7be84563476633d44d02787271
Codexophile <jaliya.chirantha@gmail.com>
2025-06-15 09:34:24 +0530

    feat: add Tippy.js default styles and adjust tooltip placement for product items

--- [tm] daraz.user.js
+++ [tm] daraz.user.js
@@ -6,6 +6,14 @@
 
   const productItemEls = document.querySelectorAll(queryForProductItems);
 
+  // 1. Add Tippy.js default styles
+  GM_addStyle(`
+    .tippy-box[data-theme~='light-border'] {
+        border: 1px solid #dadada;
+        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+    }
+  `);
+
   lazyLoad(async (productItemEl) => {
     const productLink = productItemEl.querySelector("a").href;
     const productDoc = await fetchDoc(productLink);
@@ -14,8 +22,15 @@
     );
 
     const imageContEl = generateElements(`<div class="img-cont"></div>`);
+    style(
+      imageContEl,
+      `
+      display: flex;
+      flex-wrap: wrap;
+      max-width: 1000px;`
+    );
     imageContEl.append(...thumbImgEls);
-    productItemEl.append(imageContEl);
+    // productItemEl.append(imageContEl);
 
     thumbImgEls.forEach((thumbImgEl) => {
       const src = thumbImgEl.src;
@@ -54,14 +69,6 @@
 
     //* tippy.js configurations
 
-    // 1. Add Tippy.js default styles
-    GM_addStyle(`
-    .tippy-box[data-theme~='light-border'] {
-        border: 1px solid #dadada;
-        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
-    }
-`);
-
     // 2. Find the trigger and its content element
     const triggerElement = productItemEl;
     const contentElement = imageContEl;
@@ -72,10 +79,11 @@
         content: contentElement, // Pass the DOM element directly
         allowHTML: true, // Necessary to render the HTML
         interactive: true, // Allows you to hover over the tippy itself
-        placement: "bottom-start", // Preferred placement, will adjust automatically
+        placement: "right", // Preferred placement, will adjust automatically
         theme: "light-border", // Use a pre-defined or custom theme
         animation: "fade", // A little flair
         trigger: "mouseenter", // Show on hover
+        arrow: false,
         // hideOnClick: false,  // Keep it open even if you click inside
       });
     }
