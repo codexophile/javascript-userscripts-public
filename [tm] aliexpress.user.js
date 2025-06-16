3897a1620e39c7d8850705e471e96fc6db094785
Codexophile <jaliya.chirantha@gmail.com>
2025-06-16 08:51:39 +0530

    feat: add custom search handling to redirect based on search query

--- [tm] aliexpress.user.js
+++ [tm] aliexpress.user.js
@@ -2,6 +2,16 @@
   "use strict";
   if (window.top != window.self) return; //don't run on frames or iframes
 
+  //* handling custom search
+  const urlObj = new URL(location.href);
+  if (urlObj.searchParams.has("search-query")) {
+    const searchQuery = urlObj.searchParams.get("search-query");
+    const searchQueryConverted = searchQuery.replaceAll(" ", "-");
+    const newUrl = `https://www.aliexpress.com/w/wholesale-${searchQueryConverted}.html`;
+    console.log("test");
+    location.replace(newUrl);
+  }
+
   waitForEach('[href*="/item/"]', (linkEl) => {
     linkEl.href = sanitizeLinksTraditional(linkEl.href);
   });
