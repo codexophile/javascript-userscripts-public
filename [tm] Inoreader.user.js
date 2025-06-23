9808a17cb50685fae7427457acd14af0b14cbd0e
Codexophile <jaliya.chirantha@gmail.com>
2025-06-23 08:12:07 +0530

    refactor: reorganize code structure for better readability and maintainability

--- [tm] Inoreader.user.js
+++ [tm] Inoreader.user.js
@@ -1,23 +1,42 @@
-( function () {
-    'use strict';
+(function () {
+  "use strict";
 
-    waitFor( `#show_articles_menu` ).then( ( unreadIndicator ) => {
-        document.querySelector( `.heading-wrapper > div > h2` ).prepend( unreadIndicator );
-    } );
+  //* filtering shorts
+  const shortsFilterList = ["ExplosmEntertainment"];
+  waitForEach(
+    `.article_title_link[href*="https://www.youtube.com/shorts/"]`,
+    (shortsLocatorEl) => {
+      const rssItemEl = shortsLocatorEl.closest(".ar");
+      const feedTitle = rssItemEl
+        .querySelector(`.article_tile_footer_feed_title`)
+        .textContent.trim();
+      console.log(feedTitle);
+    }
+  );
+  //*
+  // waitFor(`#show_articles_menu`).then((unreadIndicator) => {
+  //   document
+  //     .querySelector(`.heading-wrapper > div > h2`)
+  //     .prepend(unreadIndicator);
+  // });
 
-    let observer = new MutationObserver( () => {
-
-        const noMoreEl = document.querySelector( `#no_more_press_space[style="visibility: visible;"]` );
-        if ( !noMoreEl ) return; // 🛑
-
-        setTimeout( () => {
-            if ( document.querySelector( `#no_more_press_space[style="visibility: visible;"]` ) ) {
-                const kbEvent = new KeyboardEvent( 'keydown', { 'keyCode': 32, 'which': 32 } );
-                document.dispatchEvent( kbEvent );
-            }
-        }, 2000 );
-
-    } );
-    observer.observe( document.body, { childList: true, subtree: true } );
-
-} )();
+  //* auto advancing
+  waitForEach(
+    `#no_more_press_space[style="visibility: visible;"]`,
+    (noMoreIndicatorEl) => {
+      setTimeout(() => {
+        if (
+          document.querySelector(
+            `#no_more_press_space[style="visibility: visible;"]`
+          )
+        ) {
+          const kbEvent = new KeyboardEvent("keydown", {
+            keyCode: 32,
+            which: 32,
+          });
+          document.dispatchEvent(kbEvent);
+        }
+      }, 2000);
+    }
+  );
+})();
