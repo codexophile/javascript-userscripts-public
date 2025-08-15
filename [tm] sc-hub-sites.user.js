b6ec3d9579b33c062ad2217f7715e09c351f6c5d
codexophile <jaliya.chirantha@gmail.com>
2025-08-15 08:09:49 +0530

    [tm] sc-hub-sites.user.js: refactor code for consistency and improved readability

--- [tm] sc-hub-sites.user.js
+++ [tm] sc-hub-sites.user.js
@@ -1,11 +1,11 @@
 (function () {
-  "use strict";
+  'use strict';
   if (window.top != window.self) return; //don't run on frames or iframes
 
   const doodHostsQuery = getDoodHostsQuery();
 
   const itemEls = document.querySelectorAll(`.videos-list article`);
-  itemEls.forEach(async (itemEl) => {
+  itemEls.forEach(async itemEl => {
     const newContainerEl = generateElements(
       `<div class=new-container></div>`,
       itemEl
@@ -14,7 +14,10 @@
     const itemUrl = itemEl.querySelector(`a`).href;
     const doc = await fetchDoc(itemUrl);
     const iframeEls = doc.querySelectorAll(`iframe`);
-    iframeEls.forEach(async (iframeEl) => {
+    if (iframeEls.length === 0)
+      console.log('No iframe elements found in the video page.');
+
+    iframeEls.forEach(async iframeEl => {
       const iframeSrc = iframeEl.src;
       const linkEl = generateElements(
         `<a target=_blank href="${iframeSrc}">${iframeSrc}</a>`,
@@ -22,7 +25,7 @@
       );
     });
 
-    itemEl.querySelectorAll(doodHostsQuery).forEach(async (doodLinkEl) => {
+    itemEl.querySelectorAll(doodHostsQuery).forEach(async doodLinkEl => {
       const doodSBUrl = await getDoodStoryboardSrc(doodLinkEl.href);
       generateElements(
         `<img src="${doodSBUrl}" alt="Dood storyboard" style="max-width: 100px; margin-right: 10px;">`,
@@ -30,16 +33,16 @@
       );
     });
 
-    const newLinkEls = newContainerEl.querySelectorAll("a");
-    newLinkEls.forEach(async (newLinkEl) => {
+    const newLinkEls = newContainerEl.querySelectorAll('a');
+    newLinkEls.forEach(async newLinkEl => {
       const url = new URL(newLinkEl.href);
       const hostName = url.hostname;
 
       if (hostName)
-        if (hostName.includes("listeamed")) {
+        if (hostName.includes('listeamed')) {
           const doc = await fetchDoc(url);
-          const videoParentEl = doc.querySelector("video").parentElement;
-          const scriptEl = videoParentEl.querySelector("script");
+          const videoParentEl = doc.querySelector('video').parentElement;
+          const scriptEl = videoParentEl.querySelector('script');
           const PlayerConfigObj = convertPlayerConfigStringToObject(
             scriptEl.textContent
           );
