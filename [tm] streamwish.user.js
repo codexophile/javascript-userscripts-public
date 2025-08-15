8e6c706393de9a5d680d1a78efea8f0b61620aa9
codexophile <jaliya.chirantha@gmail.com>
2025-08-15 08:13:34 +0530

    __[tm] streamwish.user.js__: refactor code for consistency and improved readability

--- [tm] streamwish.user.js
+++ [tm] streamwish.user.js
@@ -1,28 +1,33 @@
 (async function () {
-  "use strict";
+  'use strict';
 
   window.close = () => {};
 
-  const videoTitle = document.title.replace(" • [Browser:Private-profile]", "");
-  setupYtDlpBtn(null, videoTitle, "#titleM3u8Link");
+  const videoTitle = document.title.replace(' • [Browser:Private-profile]', '');
+  if (location.href.includes('/e/'))
+    setupYtDlpBtn(null, videoTitle, '#titleM3u8Link');
+  if (location.href.includes('/f/')) {
+    // const downloadUrl = document.querySelector(`.submit-btn`).href;
+    setupYtDlpBtn(null, videoTitle, '.submit-btn');
+  }
 
   (async function () {
-    "use strict";
+    'use strict';
     let storyboardUrl;
 
     const thumbEl = await waitFor('.jw-time-thumb[style*="background-image"]');
-    console.log("test");
+    console.log('test');
     storyboardUrl = thumbEl.style.backgroundImage.match(/['"](.*)['"]/)[1];
 
     const $videoEl = $(`video[src]`);
-    $videoEl.data("processed", true);
-    $videoEl.prop("volume", 0.01);
-    $("#vplayer").css(`height`, `70vh`);
+    $videoEl.data('processed', true);
+    $videoEl.prop('volume', 0.01);
+    $('#vplayer').css(`height`, `70vh`);
 
     let parent;
-    if ($(".videoplayer, #vplayer").length)
+    if ($('.videoplayer, #vplayer').length)
       parent = $(`<div></div>`).insertAfter(
-        $(".videoplayer, #vplayer").first()
+        $('.videoplayer, #vplayer').first()
       );
     else parent = $(`<div></div>`).appendTo(document.body);
 
