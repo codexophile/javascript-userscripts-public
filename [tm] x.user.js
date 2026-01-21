c5f4ee17b6e67dd31747a9217e3142f11c94d425
Codexophile <jaliya.chirantha@gmail.com>
2026-01-21 19:17:46 +0530

    remove auto clicking of show more (originally intended for nsfw posts)

--- [tm] x.user.js
+++ [tm] x.user.js
@@ -1,17 +1,12 @@
 (function () {
-  "use strict";
+  'use strict';
 
   // Automatically clicking 'New post notifications for ' item
   const notifQuery = '[data-testid="notification"]';
-  waitForEach(notifQuery, (notifEl) => {
-    if (!location.href.includes("#notif")) return;
-    if (notifEl.textContent.includes("New post notifications for ")) {
+  waitForEach(notifQuery, notifEl => {
+    if (!location.href.includes('#notif')) return;
+    if (notifEl.textContent.includes('New post notifications for ')) {
       notifEl.click();
     }
   });
-
-  // Automatically clicking "show" button on censored tweets
-  const query =
-    '[role=article] [role=button]:contains("Show"), [role=button] > span:contains("Show")';
-  waitForEach(query, (showEl) => showEl.click());
 })();
