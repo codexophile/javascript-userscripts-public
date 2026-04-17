3051eb0111cd74c12af7e58a194e548926261125
Codexophile <jaliya.chirantha@gmail.com>
2026-04-17 14:20:51 +0530

    fix: change volume-related elements to fieldset for better semantics

--- [tm] video-controls/[tm] global-video-controls-main-structure.user.js
+++ [tm] video-controls/[tm] global-video-controls-main-structure.user.js
@@ -52,14 +52,14 @@ const htmlStructure = `
       <button id="buttonRotateR">⭮</button>
     </div>
 
-    <div id="volume-related-elements" class="important" style="display: flex;">
+    <fieldset id="volume-related-elements" class="important" style="display: flex;">
       <div id="volume-sliders" class="important">
         <input type="range" class="slidVolFin important vidContRange" min="0" max="0.25" step="0.001">
         <input type="range" class="slidVolExt important vidContRange" min="0.25" max="1" step="0.001" style="display: none;" title="Extended volume (0.25 to 1)">
       </div>
       <input type="number" title="Volume" step="0.001" class="numinp" id="volDisp">
       <button id="muteButton" for="volDisp">🔊</button>
-    </div>
+    </fieldset>
     
     <input type="range" id="progress" class="important vidContRange" min="0" max="100" step="0.001" value="0">
 
