ee76e46ecf85c2f9eee823bf723fc0d44ffcea9f
Codexophile <jaliya.chirantha@gmail.com>
2026-04-19 09:49:09 +0530

    refactor: remove unused addExternalLinks function and clean up code

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -20,49 +20,12 @@
   });
   observer.observe(document.body, { childList: true, subtree: true });
 
-  //* External links
-  addExternalLinks();
-
+  //? I'm not sure what this is 👇
   const $moreFromSectionEl = $(`[data-testid="more-from-section"]`);
   $moreFromSectionEl.insertBefore('[data-testid="contribution"]');
 
   //*____________________
   if (location.href.includes('https://m.')) {
     location.replace(location.href.replace('https://m.', 'https://www.'));
-  }
-
-  async function addExternalLinks() {
-    const titleMatch = location.href.match(/\/title\/(tt\d+)/);
-
-    if (!titleMatch) return;
-
-    const el = await waitFor(`[data-testid="hero__pageTitle"] ~ ul`);
-    const titleEl = await waitFor(`[data-testid="hero__pageTitle"]`);
-    const title = encodeURIComponent(titleEl.textContent);
-    const yearEl = await waitFor(`[href*="/releaseinfo"]`);
-    const year = yearEl.textContent;
-
-    //* YTS
-    const titleId = titleMatch[1];
-    addExtLink('https://yts.lt/', 'browse-movies/' + titleId);
-
-    //* Leet
-    addExtLink('https://1337x.to/', `search/${title}+${year}/1/`);
-
-    function addExtLink(
-      urlBase,
-      urlRest,
-      imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${urlBase}`,
-    ) {
-      generateElements(
-        `
-            <li class=ipc-inline-list__item>
-                <a target=_blank href=${urlBase}${urlRest}>
-                    <img style='width: 32px' src='${imgSrc}'>
-                </a>
-            </li>`,
-        el,
-      );
-    }
   }
 })();
