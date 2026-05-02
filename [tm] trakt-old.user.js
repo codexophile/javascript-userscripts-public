4416353cadd3cffa135d32b2a8a130448fc5c215
Codexophile <jaliya.chirantha@gmail.com>
2026-05-02 20:34:21 +0530

    feat: refactor to use async function and enhance movie link redirection

--- [tm] trakt-old.user.js
+++ [tm] trakt-old.user.js
@@ -1,5 +1,14 @@
-(function () {
+(async function () {
   'use strict';
+
+  waitForEach('#recommendations-wrapper [href^="/movies/"]', movieLinkEl => {
+    movieLinkEl.href += '#redirectIMDb';
+  });
+  const urlHash = window.location.hash;
+  if (urlHash === '#redirectIMDb') {
+    const imdbLinkEl = await waitFor('#external-link-imdb');
+    location.href = imdbLinkEl.href;
+  }
 
   waitForEach('[href^="/people/"]', peopleLinkEl => {
     const url = new URL(peopleLinkEl.href);
