dd1974255978dacf8a4fdabbbf2c532fe1e4015b
Codexophile <jaliya.chirantha@gmail.com>
2026-05-28 23:56:30 +0530

    fix: standardize quotes and add alert for Tampermonkey users

--- [tm] instagram auto liker.user.js
+++ [tm] instagram auto liker.user.js
@@ -1,14 +1,16 @@
 (function () {
-  "use strict";
+  'use strict';
   if (window.top != window.self) return; //don't run on frames or iframes
 
-  waitForEach("[aria-label='Like']", (locatorEl) => {
-    if (location.href !== "https://www.instagram.com/") return;
+  alert('Disable this from Tampermonkey');
+
+  waitForEach("[aria-label='Like']", locatorEl => {
+    if (location.href !== 'https://www.instagram.com/') return;
     if (
       locatorEl
-        .closest("article")
-        .querySelector("div")
-        .textContent.includes("Suggested for you")
+        .closest('article')
+        .querySelector('div')
+        .textContent.includes('Suggested for you')
     )
       return;
     const likeBtnEl = locatorEl.parentElement;
