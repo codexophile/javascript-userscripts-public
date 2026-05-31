0d60979936e7a274160f0e00c9e2f751f05c2a7c
Codexophile <jaliya.chirantha@gmail.com>
2026-05-31 14:30:20 +0530

    feat: implement code section toggle functionality

--- [tm] github.user.js
+++ [tm] github.user.js
@@ -1,9 +1,18 @@
-( function () {
-    'use strict'
+(function () {
+  'use strict';
 
-    window.navigation.addEventListener( "navigate", () => {
-        $( `article a` ).not( '[href*="#"]' ).attr( 'target', '_blank' )
-        $( `article` ).parent().parent()[ 0 ]?.scrollIntoView() //* scrolls post content into view
-    } )
+  //* code section toggle
+  waitForEach('[data-hpc=true]:not([class])', codeSectionEl => {
+    codeSectionEl.style.display = 'none';
+    const toggleBtnEl = generateElements(`<button>toggle</button>`);
+    codeSectionEl.before(toggleBtnEl);
+    toggleBtnEl.addEventListener('click', () => {
+      toggle(codeSectionEl);
+    });
+  });
 
-} )()
+  window.navigation.addEventListener('navigate', () => {
+    $(`article a`).not('[href*="#"]').attr('target', '_blank');
+    $(`article`).parent().parent()[0]?.scrollIntoView(); //* scrolls post content into view
+  });
+})();
