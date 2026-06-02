730b88172fd759035bc2809b43a2571ef543247b
Codexophile <jaliya.chirantha@gmail.com>
2026-06-02 18:04:37 +0530

    feat: add functionality to store closed caption channel IDs

--- [tm] youtube.user.js
+++ [tm] youtube.user.js
@@ -1,6 +1,26 @@
 (async function () {
   'use strict';
 
+  //* moving closed captions
+  (async function () {
+    'use strict';
+
+    if (!location.href.match(/\/watch\?v=/)) return;
+
+    const { addButton } = await Collapsible();
+    addButton('➕', null, () => {
+      addUniqueStoredValue('closedCaptionChannels', getChannelId());
+    });
+  })();
+
+  async function getChannelId() {
+    const authorEl = await waitFor('[itemprop=author] > [itemprop=url]');
+    const channelId = authorEl.href
+      .match(/\/\/www\.youtube\.com\/(.+?)(\/|$)/)[1]
+      .slice(1);
+    return channelId;
+  }
+
   // markAndFilter( 'ytd-rich-item-renderer', 'a[href^="/watch?v="]', 'href', /\?v=(...........)/ );
 
   fixUrl();
