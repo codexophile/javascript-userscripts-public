23d338170263ff8cca699fb83fcb79ae2b12c44e
Codexophile <jaliya.chirantha@gmail.com>
2026-06-13 21:02:44 +0530

    feat: refactor highlightEpisode function and add value change listeners for season and episode numbers

--- [tm] trakt-old.user.js
+++ [tm] trakt-old.user.js
@@ -2,21 +2,22 @@
   'use strict';
 
   if (location.href.includes('https://www.ratingraph.com/tv-shows/')) {
-    const seasonNumber = +GM_getValue('seasonNumber');
-    const episodeNumber = +GM_getValue('episodeNumber');
-    if (!(seasonNumber && episodeNumber)) return;
-    await waitFor('#graph_show_episodes_average_rating .highcharts-series');
-    const seasonEl = document.querySelectorAll(
-      '#graph_show_episodes_average_rating .highcharts-markers.highcharts-scatter-series',
-    )[seasonNumber - 1];
-    const episodeEl = seasonEl.children[episodeNumber - 1];
-    style(
-      episodeEl,
-      `
-      outline: 2px solid red;
-    `,
-    );
-    return;
+    GM_addValueChangeListener('seasonNumber', highlightEpisode);
+    GM_addValueChangeListener('episodeNumber', highlightEpisode);
+    highlightEpisode();
+
+    async function highlightEpisode() {
+      const seasonNumber = +GM_getValue('seasonNumber');
+      const episodeNumber = +GM_getValue('episodeNumber');
+      if (!(seasonNumber && episodeNumber)) return;
+      await waitFor('#graph_show_episodes_average_rating .highcharts-series');
+      const seasonEl = document.querySelectorAll(
+        '#graph_show_episodes_average_rating .highcharts-markers.highcharts-scatter-series',
+      )[seasonNumber - 1];
+      const episodeEl = seasonEl.children[episodeNumber - 1];
+      style(episodeEl, `outline: 2px solid red;`);
+      return;
+    }
   }
 
   //*
