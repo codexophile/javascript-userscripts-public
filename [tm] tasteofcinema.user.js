c38d8cb4e1dba8ae16ef4dae0b193ccf25758f2f
Codexophile <jaliya.chirantha@gmail.com>
2026-06-29 10:47:16 +0530

    feat: add script to generate IMDb links from article headers

---
+++ [tm] tasteofcinema.user.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+  if (window.top != window.self) return; //don't run on frames or iframes
+
+  const headerEls = document.querySelectorAll(
+    '.entry-content span[style*="font-size: 20px;"]',
+  );
+  headerEls.forEach(headerEl => {
+    const headerText = headerEl.textContent.trim();
+    const matches = headerText.match(/\d\d\. (.+?) \((\d{4})\)/);
+    if (!matches) return;
+    const title = matches[1];
+    const year = matches[2];
+    const query = `${title} ${year}`;
+    const imdbLinkEl = generateElements(`<a>Imdb</a>`, headerEl);
+    imdbLinkEl.setAttribute(
+      'href',
+      `https://www.google.com/search?btnI=1&q=${query}%20site:imdb.com/title`,
+    );
+    imdbLinkEl.setAttribute('target', '_blank');
+    style(
+      imdbLinkEl,
+      `
+      margin-left: 10px;
+      color: #fff;
+    `,
+    );
+  });
+})();
