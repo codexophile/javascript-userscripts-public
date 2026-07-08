b12f03c4d2cfd85e35f5f0dee5fb9d4035f32474
Codexophile <jaliya.chirantha@gmail.com>
2026-07-08 14:48:39 +0530

    reintroduce external links (yts and leetx) for title pages

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -1,6 +1,44 @@
 (function () {
   'use strict';
 
+  //* external links
+  (async function () {
+    'use strict';
+
+    const titleMatch = location.href.match(/\/title\/(tt\d+)/);
+
+    if (!titleMatch) return;
+
+    const el = await waitFor(`[data-testid="hero__pageTitle"] ~ ul`);
+    const titleEl = await waitFor(`[data-testid="hero__pageTitle"]`);
+    const title = encodeURIComponent(titleEl.textContent);
+    const yearEl = await waitFor(`[href*="/releaseinfo"]`);
+    const year = yearEl.textContent;
+
+    //* YTS
+    const titleId = titleMatch[1];
+    addExtLink('https://yts.gg/', 'browse-movies/' + titleId);
+
+    //* Leet
+    addExtLink('https://1337x.to/', `search/${title}+${year}/1/`);
+
+    function addExtLink(
+      urlBase,
+      urlRest,
+      imgSrc = `https://www.google.com/s2/favicons?sz=64&domain=${urlBase}`,
+    ) {
+      generateElements(
+        `
+            <li class=ipc-inline-list__item>
+                <a target=_blank href=${urlBase}${urlRest}>
+                    <img style='width: 32px' src='${imgSrc}'>
+                </a>
+            </li>`,
+        el,
+      );
+    }
+  })();
+
   //* tooltips for genre/interests pill elements
   const genrePillEls = document.querySelectorAll(
     '[data-testid="interests"] .ipc-chip',
