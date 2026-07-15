4e8eae6a74c68fb1d894d421684add85294ae4b8
Codexophile <jaliya.chirantha@gmail.com>
2026-07-15 23:58:55 +0530

    style: format code for consistency and readability

--- [tm] private-profile-title-setter.user.js
+++ [tm] private-profile-title-setter.user.js
@@ -1,11 +1,14 @@
-( function () {
-    'use strict'
+(function () {
+  'use strict';
 
-    const string = '[Browser:Private-profile]'
-    let titleObserver = new MutationObserver( () => {
-        if ( document.title.includes( string ) ) return // 🛑
-        document.title = `${ document.title } • ${ string }`
-    } )
-    titleObserver.observe( document.head, { childList: true, subtree: true } )
+  const string = '[Browser:Private-profile]';
+  let titleObserver = new MutationObserver(() => {
+    if (document.title.includes(string)) return; // 🛑
+    document.title = `${document.title} • ${string}`;
+  });
+  titleObserver.observe(document.head, { childList: true, subtree: true });
 
-} )()
+  waitForEach('video', videoEl => {
+    videoEl.volume = 0.01;
+  });
+})();
