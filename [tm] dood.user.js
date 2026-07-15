5ba21fb1702b23d02f7ec8d1ebbbe4319d735249
Codexophile <jaliya.chirantha@gmail.com>
2026-07-15 23:57:51 +0530

    fix: standardize quote usage and improve code readability

--- [tm] dood.user.js
+++ [tm] dood.user.js
@@ -1,32 +1,39 @@
-(function () {
-  "use strict";
+(async function () {
+  'use strict';
   if (window.top != window.self) return; //don't run on frames or iframes
 
   disableConsoleClear();
+  window.console.clear = null;
+  window.close = null;
 
-  if (location.href.includes("/d/") && !location.href.includes("#noRedirect")) {
+  if (location.href.includes('/d/') && !location.href.includes('#noRedirect')) {
     window.stop();
-    const iframeSrc = document.querySelector("iframe").src;
+    const iframeSrc = document.querySelector('iframe').src;
     location.href = iframeSrc;
     // location.href = location.href.replace( '/d/', '/e/' )
   }
-  if (location.href.includes("/e/")) {
+  if (location.href.includes('/e/')) {
     GM_addStyle(`iframe { display: none !important; }`);
 
-    document.querySelector(`#os_player`).style.height = "72vh";
+    document.querySelector(`#os_player`).style.height = '72vh';
 
     $(
-      `<a href="${location.href.replace("/e/", "/d/")}#noRedirect"> ${
+      `<a href="${location.href.replace('/e/', '/d/')}#noRedirect"> ${
         document.title
-      } </a>`
+      } </a>`,
     ).prependTo(document.body);
-    if (!document.querySelector(`[name="og:image"]`)) return; // 🛑
-    let imageUrl = document
-      .querySelector(`[name="og:image"]`)
-      .getAttribute("content");
-    imageUrl = imageUrl.replace("/splash/", "/slides/");
-    imageUrl = imageUrl.replace("/cover/", "/slides/");
-    imageUrl = imageUrl.replace(/-.+?\./, ".");
+
+    // if (!document.querySelector(`[name="og:image"]`)) return; // 🛑
+    // let imageUrl = document
+    //   .querySelector(`[name="og:image"]`)
+    //   .getAttribute('content');
+    // imageUrl = imageUrl.replace('/splash/', '/slides/');
+    // imageUrl = imageUrl.replace('/cover/', '/slides/');
+    // imageUrl = imageUrl.replace(/-.+?\./, '.');
+
+    const imageUrl = await getDoodStoryboardSrc(
+      location.href.replace('/e/', '/d/'),
+    );
     const vidOnPage = document.querySelector(`video`);
 
     storyboard({
