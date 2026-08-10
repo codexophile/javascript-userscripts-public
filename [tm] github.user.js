c0625c128819cd3fb79bc27c9742b87d1a75e0a0
Codexophile <jaliya.chirantha@gmail.com>
2026-08-10 19:48:13 +0530

    chore: unnessary code

--- [tm] github.user.js
+++ [tm] github.user.js
@@ -10,9 +10,4 @@
       toggle(codeSectionEl);
     });
   });
-
-  window.navigation.addEventListener('navigate', () => {
-    $(`article a`).not('[href*="#"]').attr('target', '_blank');
-    $(`article`).parent().parent()[0]?.scrollIntoView(); //* scrolls post content into view
-  });
 })();
