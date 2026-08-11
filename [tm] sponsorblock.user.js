54ddc2075215f0ddfbce70be4951c4ebb531fd54
Codexophile <jaliya.chirantha@gmail.com>
2026-08-12 00:40:43 +0530

    feat: add official youtube thumbnails

---
+++ [tm] sponsorblock.user.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  const SELECTOR = '[href^="/video/"]';
+
+  GM_addStyle(`
+    ${SELECTOR} {
+      text-overflow: unset;
+      white-space: unset;
+      overflow: unset;
+    }
+  `);
+
+  waitForEach(SELECTOR, el => {
+    const videoId = el.href.match(/\/video\/(.+)/)[1];
+    if (!videoId) return;
+    const thumbSrc = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+    generateElements(
+      `<img src="${thumbSrc}" style="width: 100%; margin-top: 5px;" />`,
+      el,
+    );
+  });
+})();
