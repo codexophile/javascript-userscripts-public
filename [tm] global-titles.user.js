da8d98a7ba4448655a65d213f5a298924b7d9af6
Codexophile <jaliya.chirantha@gmail.com>
2026-08-14 21:54:33 +0530

    feat

--- [tm] global-titles.user.js
+++ [tm] global-titles.user.js
@@ -40,6 +40,7 @@
         <button id="btn-toggle" class="btn">⇰</button>
         <button id="btn-copy-title" class="btn">T</button>
         <button id="btn-copy-url" class="btn">U</button>
+        <button id="btn-open-in-new" class="btn">↗</button>
       </div>
       <div id="content">
       </div>
@@ -48,6 +49,10 @@
     document.body,
   );
   const titleCntEl = mainCntEl.querySelector('#content');
+
+  mainCntEl.querySelector('#btn-open-in-new').addEventListener('click', () => {
+    window.open(window.location.href, '_blank');
+  });
 
   let observer = new MutationObserver(main);
   observer.observe(titleEl, { childList: true, subtree: true });
