6074605da1c56bc374801611610a43d35d0925c4
Codexophile <jaliya.chirantha@gmail.com>
2026-08-14 20:35:54 +0530

    improvements

--- [tm] global-titles.user.js
+++ [tm] global-titles.user.js
@@ -2,21 +2,25 @@
   'use strict';
 
   const titleEl = await waitFor(`title`);
-  let observer = new MutationObserver(main);
-  observer.observe(titleEl, { childList: true, subtree: true });
 
   GM_addStyle(`
     #main-cnt {
+      display: flex;
       position: fixed;
       bottom: 0;
       right: 0;
     }
+    #btns-cnt {
+      display: flex;
+    }
   `);
 
+  await waitFor('body');
   const mainCntEl = generateElements(
     `
     <div id="main-cnt">
       <div id="btns-cnt">
+        <button id="btn-toggle" class="btn">⇰</button>
         <button id="btn-copy-title" class="btn">T</button>
         <button id="btn-copy-url" class="btn">U</button>
       </div>
@@ -28,8 +32,12 @@
   );
   const titleCntEl = mainCntEl.querySelector('#content');
 
+  let observer = new MutationObserver(main);
+  observer.observe(titleEl, { childList: true, subtree: true });
+
   function main() {
     const title = document.title;
+    if (!titleCntEl) return;
     titleCntEl.innerHTML = title;
   }
 })();
