3d3fb959c37951588d3292c5d2a69db4d37ef98d
Codexophile <jaliya.chirantha@gmail.com>
2026-08-21 20:30:17 +0530

    auto redirect when apex is invalid

--- [tm] crt.name.user.js
+++ [tm] crt.name.user.js
@@ -3,6 +3,13 @@
 
   if (!location.href.includes('/v1/search?apex=')) return;
   const preEl = document.querySelector('pre');
+  const preElText = preEl.textContent.trim();
+  const match = preElText.match(
+    /invalid apex: not an apex \(eTLD\+1 is (.+)\)/,
+  );
+  if (match) {
+    location.replace(`https://crt.name/v1/search?apex=${match[1]}`);
+  }
   preEl.innerHTML = preEl.textContent
     .trim()
     .split('\n')
