8c08f7a1aa8bafb1d13661d157466c9534577070
Codexophile <jaliya.chirantha@gmail.com>
2026-08-22 11:38:11 +0530

    cleanup

--- [tm] activitywatch.user.js
+++ [tm] activitywatch.user.js
@@ -1,25 +1,17 @@
-( async function () {
-    'use strict';
-    if ( window.top != window.self ) return; //don't run on frames or iframes
+(async function () {
+  'use strict';
 
-    const lb = await waitFor( `select.custom-select-sm` );
-    sortOptions( lb );
+  const lb = await waitFor(`select.custom-select-sm`);
+  sortOptions(lb);
 
-    function sortOptions ( selectEl ) {
-
-        const options = Array.from( selectEl.options );
-
-        // Sort options alphabetically by the visible text
-        options.sort( ( a, b ) => a.text.localeCompare( b.text ) );
-
-        // Remove existing options
-        while ( selectEl.firstChild ) {
-            selectEl.removeChild( selectEl.firstChild );
-        }
-
-        // Append sorted options back to the select
-        options.forEach( option => selectEl.appendChild( option ) );
+  function sortOptions(selectEl) {
+    const optionsArr = Array.from(selectEl.options);
+    optionsArr.sort((a, b) => a.text.localeCompare(b.text));
+    // Remove existing options
+    while (selectEl.firstChild) {
+      selectEl.removeChild(selectEl.firstChild);
     }
-
-
-} )();
+    // Append sorted options back to the select
+    optionsArr.forEach(option => selectEl.appendChild(option));
+  }
+})();
