056a01a1f31496e66c4e28684564efa0bd48d266
Codexophile <jaliya.chirantha@gmail.com>
2026-08-28 20:05:24 +0530

    fixes

--- [tm] imdb.user.js
+++ [tm] imdb.user.js
@@ -28,7 +28,12 @@
       '[data-testid="hero-parent"] > div > div:first-child',
     )[2];
 
-    style(targetParentEl, `flex-wrap: wrap;`);
+    style(
+      targetParentEl,
+      `
+      flex-wrap: wrap;
+    `,
+    );
 
     const contEl = generateElements(`<div></div>`, targetParentEl);
     style(
