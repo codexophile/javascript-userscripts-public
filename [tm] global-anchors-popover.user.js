2f743900e7efd5febe32a4360de3839306f1a381
Codexophile <jaliya.chirantha@gmail.com>
2026-08-29 19:10:27 +0530

    svg fixes

--- [tm] global-anchors-popover.user.js
+++ [tm] global-anchors-popover.user.js
@@ -6,7 +6,7 @@
   const POPOVER_ID = 'cdx-main-popover';
   const ELEMENT_WIDTH = '30px';
   const svgsObj = {
-    'open-in-new': `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>`,
+    'open-in-new': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>`,
   };
 
   let openTimer = null;
