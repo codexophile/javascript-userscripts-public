46f979be7d5b7259c269e626bfd251690489f68d
Codexophile <jaliya.chirantha@gmail.com>
2026-08-29 19:16:35 +0530

    size fixes

--- [tm] global-anchors-popover.user.js
+++ [tm] global-anchors-popover.user.js
@@ -4,7 +4,7 @@
   const OPEN_DELAY = 100;
   const CLOSE_DELAY = 200;
   const POPOVER_ID = 'cdx-main-popover';
-  const ELEMENT_WIDTH = '30px';
+  const ELEMENT_WIDTH = '20px';
   const svgsObj = {
     'open-in-new': `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>`,
   };
@@ -30,10 +30,13 @@
       box-shadow: 0 4px 12px rgba(0,0,0,0.15);
 
       button {
+        padding: 0;
         width: ${ELEMENT_WIDTH};
         height: ${ELEMENT_WIDTH};
       }
-        
+
+    }
+
   `);
 
   popoverEl.addEventListener('mouseenter', () => clearTimeout(closeTimer));
